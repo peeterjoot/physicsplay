51b29238f6017be715b490e38c50f4eb2dbaf2b1
Peeter <Peeter>
2008-12-14 06:01:54 +0000

    *** empty log message ***
    Former-commit-id: 982cca9a50178b16e73c81cc3c241b383ce8782f
    Former-commit-id: d05747aadaacc2df489a2479f8ef47da047a7009

--- notes/gamma-matrix/matrix.cpp
+++ notes/gamma-matrix/matrix.cpp
@@ -90,25 +90,40 @@ int main()
    printit("\\gamma^3 = ", gamma2) ;
    printit("\\gamma^4 = ", gamma3) ;
 
-#if 0
-   cmatrix g11(4,4) ;
-   g11 = prod( gamma0, gamma0 ) ;
-   printit( "\\gamma^1 \\gamma^1 = ", g11 ) ;
+// BEGIN:!perl generate_prods.pl > u
 
-   cmatrix g22(4,4) ;
-   g22 = prod( gamma1, gamma1 ) ;
-   printit( "\\gamma^2 \\gamma^2 = ", g22 ) ;
+   {
+      cmatrix t12(4,4) ;
+      cmatrix g123(4,4) ;
 
-   cmatrix g33(4,4) ;
-   g33 = prod( gamma2, gamma2 ) ;
-   printit( "\\gamma^3 \\gamma^3 = ", g33 ) ;
+      t12 = prod( gamma1, gamma2 ) ;
+      g123 = prod( t12, gamma3 ) ;
 
-   cmatrix g44(4,4) ;
-   g44 = prod( gamma3, gamma3 ) ;
-   printit( "\\gamma^4 \\gamma^4 = ", g44 ) ;
-#endif
+      printit( "\\gamma^1 \\gamma^2 \\gamma^3 = ", g123 ) ;
+   }
 
-// BEGIN:!perl t > u
+
+   {
+      cmatrix t23(4,4) ;
+      cmatrix g230(4,4) ;
+
+      t23 = prod( gamma2, gamma3 ) ;
+      g230 = prod( t23, gamma0 ) ;
+
+      printit( "\\gamma^2 \\gamma^3 \\gamma^0 = ", g230 ) ;
+   }
+
+
+   {
+      cmatrix t30(4,4) ;
+      cmatrix g301(4,4) ;
+
+      t30 = prod( gamma3, gamma0 ) ;
+      g301 = prod( t30, gamma1 ) ;
+
+      printit( "\\gamma^3 \\gamma^0 \\gamma^1 = ", g301 ) ;
+   }
+
 
    cmatrix g00(4,4) ;
    g00 = prod( gamma0, gamma0 ) ;
@@ -128,6 +143,19 @@ int main()
    cmatrix g33(4,4) ;
    g33 = prod( gamma3, gamma3 ) ;
    printit( "\\gamma^3 \\gamma^3 = ", g33 ) ;
+
+
+
+
+   {
+      cmatrix t01(4,4) ;
+      cmatrix g012(4,4) ;
+
+      t01 = prod( gamma0, gamma1 ) ;
+      g012 = prod( t01, gamma2 ) ;
+
+      printit( "\\gamma^0 \\gamma^1 \\gamma^2 = ", g012 ) ;
+   }
 
 
    cmatrix g01(4,4) ;
@@ -189,7 +217,20 @@ int main()
    g32 = prod( gamma3, gamma2 ) ;
    printit( "\\gamma^3 \\gamma^2 = ", g32 ) ;
 
-// END:!perl t > u
+
+// END:!perl generate_prods.pl > u
+
+   {
+      cmatrix t12(4,4) ;
+      cmatrix t123(4,4) ;
+      cmatrix g0123(4,4) ;
+
+      t12 = prod( gamma1, gamma2 ) ;
+      t123 = prod( t12, gamma3 ) ;
+      g0123 = prod( gamma0, t123 ) ;
+
+      printit( "\\gamma^0 \\gamma^1 \\gamma^2 \\gamma^3 = ", g0123 ) ;
+   }
 
    return 0 ;
 }
