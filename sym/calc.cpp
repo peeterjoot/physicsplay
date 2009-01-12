8991ac4214cbe0be69a36bb74a85871acf86289d
Peeter <Peeter>
2009-01-12 02:06:37 +0000

    *** empty log message ***
    Former-commit-id: 7bc7e60a71329ee921ae31e1da0543a9a10e38e0
    Former-commit-id: 31242142a71e42597d5a241575adca291fb26fe6

--- sym/calc.cpp
+++ sym/calc.cpp
@@ -3,8 +3,8 @@
  *
  * Peeter Joot (peeter.joot@gmail.com).
  *
- * $Date: 2009/01/12 01:06:45 $
- * $Revision: 1.1 $
+ * $Date: 2009/01/12 02:06:37 $
+ * $Revision: 1.2 $
  */
 
 #include <iostream>
@@ -13,8 +13,71 @@ using namespace std ;
 
 #include "expression.h"
 
+// calculate coeff's of (1 + x^2)/(1 + x^4) = A/() + B/() + C/() + D/()
+void tc1(void)
+{
+   expression ri( term("\\sqrt{i}") ) ;
+   expression x( term("x") ) ;
+   expression i( term("i") ) ;
+   expression iri( term("i") ) ; iri *= ri ;
+   expression A( term("A") ) ;
+   expression B( term("B") ) ;
+   expression C( term("C") ) ;
+   expression D( term("D") ) ;
+
+   expression q1(x) ;
+   q1 -= ri ;
+   q1 *= A ;
+//   q1.reduce() ;
+//   cout << q1.toString() ;
+
+   {
+      expression q2(x) ;
+      q2 += ri ;
+      q2 *= B ;
+//   q2.reduce() ;
+//   cout << q2.toString() ;
+
+      q1 += q2 ;
+   }
+
+   expression f1(x) ;
+   f1 *= x ;
+   f1 += i ;
+   f1 *= q1 ;
+   f1.reduce() ;
+//   cout << f1.toString() ;
+
+//   q1.reduce() ;
+//   cout << q1.toString() ;
+
+   expression q3(x) ; q3 -= iri ; q3 *= C ;
+   {
+      expression q4(x) ; q4 += iri ; q4 *= D ;
+
+      q3 += q4 ;
+//   q3.reduce() ;
+//   cout << q3.toString() ;
+   }
+
+   {
+      expression f2(x) ;
+      f2 *= x ;
+      f2 -= i ;
+//   f2.reduce() ;
+//   cout << f2.toString() ;
+      f2 *= q3 ;
+
+      f1 += f2 ;
+   }
+
+ f1.reduce() ;
+   cout << f1.toString() ;
+}
+
 int main(int argc, char*argv[])
 {
+   tc1() ;
 
    return 0 ;
 }
