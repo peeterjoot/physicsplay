142d664be909670fd3742f93ce4bb4dc1957e7d6
Peeter <Peeter>
2009-01-12 01:06:45 +0000

    Initial revision
    Former-commit-id: 10976c81184d0d8bdc90a906eb324b2b5e953cc9
    Former-commit-id: d20a0d2bc6ed49e58354d25911a32a5eb0b8fd06

---
+++ sym/calc.cpp
@@ -0,0 +1,20 @@
+/*
+ * Simple symbolic calculator.
+ *
+ * Peeter Joot (peeter.joot@gmail.com).
+ *
+ * $Date: 2009/01/12 01:06:45 $
+ * $Revision: 1.1 $
+ */
+
+#include <iostream>
+
+using namespace std ;
+
+#include "expression.h"
+
+int main(int argc, char*argv[])
+{
+
+   return 0 ;
+}
