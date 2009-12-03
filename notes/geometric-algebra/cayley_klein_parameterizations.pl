9246f559fb0f1b9125892053baa8622d93edefbe
Peeter <Peeter>
2009-12-03 03:24:40 +0000

    *** empty log message ***
    Former-commit-id: bfdbacc0566545b0b6caa05bfd68bca84574f257
    Former-commit-id: 6753e34d37c32655e8b69dc4b6b73fbdbe702f2e

--- notes/geometric-algebra/cayley_klein_parameterizations.pl
+++ notes/geometric-algebra/cayley_klein_parameterizations.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-my $s = q(
+my $s0 = q(
 Let's write
 \begin{align*}
 R &= \A + \D\Be_{21} + \S \Be_{32} + \F \Be_{31} \\\\
@@ -22,45 +22,110 @@ R =
 \end{align*}
 ) ;
 
+my $s1 = q(
+\begin{align*}
+R &= \A - \D\Be_{21} - \S \Be_{32} - \F \Be_{31} \\\\
+\A &= \cos\left(\frac{\theta}{2}\right) \cos\left( \inv{2}\left(\psi+\phi\right) \right) \\\\
+\D &= -\cos\left(\frac{\theta}{2}\right) \sin\left( \inv{2}\left(\psi+\phi\right) \right) \\\\
+\S &= -\sin\left(\frac{\theta}{2}\right) \cos\left( \inv{2}\left(\psi-\phi\right) \right) \\\\
+\F &= -\sin\left(\frac{\theta}{2}\right) \sin\left( \inv{2}\left(\psi-\phi\right) \right) \\\\
+\end{align*}
+
+\begin{align*}
+\begin{bmatrix}
+ - \F^2 + \S^2 - \D^2 + \A^2 & - 2  \S \F + 2 \A \D & + 2 \D \S + 2 \A \F \\\\
+ - 2 \S \F - 2 \A \D & + \F^2 - \S^2 - \D^2 + \A^2 & - 2 \D \F + 2 \A \S \\\\
+ + 2 \D \S - 2 \A \F & - 2 \D \F - 2 \A \S & - \F^2 - \S^2 + \D^2 + \A^2 \\\\
+\end{bmatrix}
+\end{align*}
+) ;
+
+my $s2 = q(
+\begin{align*}
+R &= \A + \S\Be_{21} - \D \Be_{32} - \F \Be_{31} \\\\
+\A &= \cos\left(\frac{\theta}{2}\right) \cos\left( \inv{2}\left(\psi+\phi\right) \right) \\\\
+\S &= \cos\left(\frac{\theta}{2}\right) \sin\left( \inv{2}\left(\psi+\phi\right) \right) \\\\
+\D &= -\sin\left(\frac{\theta}{2}\right) \cos\left( \inv{2}\left(\psi-\phi\right) \right) \\\\
+\F &= -\sin\left(\frac{\theta}{2}\right) \sin\left( \inv{2}\left(\psi-\phi\right) \right) \\\\
+\end{align*}
+#endif
+
+\begin{align*}
+\begin{bmatrix}
+ - \F^2 + \D^2 - \S^2 + \A^2 & - 2 \D \F - 2 \A \S & - 2 \S \D + 2 \A \F \\\\
+ - 2 \D \F + 2 \A \S & + \F^2 - \D^2 - \S^2 + \A^2 & + 2 \S \F + 2 \A \D \\\\
+ - 2 \S \D - 2 \A \F & + 2 \S \F - 2 \A \D & - \F^2 - \D^2 + \S^2 + \A^2 \\\\
+\end{bmatrix}
+\end{align*}
+) ;
+
+my $s3 = q(
+
+\begin{align*}
+R &= \A - \S\Be_{21} + \D \Be_{32} - \F \Be_{31} \\\\
+\A &= \cos\left(\frac{\theta}{2}\right) \cos\left( \inv{2}\left(\psi+\phi\right) \right) \\\\
+\S &= -\cos\left(\frac{\theta}{2}\right) \sin\left( \inv{2}\left(\psi+\phi\right) \right) \\\\
+\D &= \sin\left(\frac{\theta}{2}\right) \cos\left( \inv{2}\left(\psi-\phi\right) \right) \\\\
+\F &= -\sin\left(\frac{\theta}{2}\right) \sin\left( \inv{2}\left(\psi-\phi\right) \right) \\\\
+\end{align*}
+
+\begin{align*}
+\begin{bmatrix}
+ - \F^2 + \D^2 - \S^2 + \A^2 & + 2 \D \F + 2 \A \S & - 2  \S \D + 2 \A \F \\
++  2 \D \F - 2  \A \S & +  \F^2 - \D^2 - \S^2 + \A^2 & - 2  \S \F - 2  \A \D \\
+ - 2  \S \D - 2  \A \F & - 2  \S \F + 2 \A \D & - \F^2 - \D^2 + \S^2 + \A^2 \\
+\end{bmatrix}
+\end{align*}
+) ;
+
+my $s = q(
+\begin{align*}
+R &= \A - \S\Be_{21} - \D \Be_{32} + \F \Be_{31} \\\\
+\A &= \cos\left(\frac{\theta}{2}\right) \cos\left( \inv{2}\left(\psi+\phi\right) \right) \\\\
+\S &= -\cos\left(\frac{\theta}{2}\right) \sin\left( \inv{2}\left(\psi+\phi\right) \right) \\\\
+\D &= -\sin\left(\frac{\theta}{2}\right) \cos\left( \inv{2}\left(\psi-\phi\right) \right) \\\\
+\F &= \sin\left(\frac{\theta}{2}\right) \sin\left( \inv{2}\left(\psi-\phi\right) \right) \\\\
+\end{align*}
+
+\begin{align*}
+\begin{bmatrix}
+ - \F^2 + \D^2 - \S^2 + \A^2 & + 2 \D \F + 2 \A \S & + 2 \S \D - 2 \A \F \\\\
++ 2 \D \F - 2 \A \S & + \F^2 - \D^2 - \S^2 + \A^2 & + 2 \S \F + 2 \A \D \\\\
++ 2 \S \D + 2 \A \F & + 2 \S \F - 2 \A \D & - \F^2 - \D^2 + \S^2 + \A^2 \\\\
+\end{bmatrix}
+\end{align*}
+) ;
+
 #print $s ;
 
 my @a = (qw(alpha beta gamma delta)) ;
-#for ( $i = 0 ; $i < 4 ; $i++ )
-#{
-#   for ( $j = 0 ; $j < 4 ; $j++ )
-#   {
-#      for ( $k = 0 ; $k < 4 ; $k++ )
-#      {
-#         for ( $l = 0 ; $l < 4 ; $l++ )
-#         {
-#            if ( $a[$i] != $a[$j] 
-#            &&   $a[$i] != $a[$k] 
-#            &&   $a[$i] != $a[$l] 
-#            &&   $a[$j] != $a[$k] 
-#            &&   $a[$j] != $a[$l] 
-#            &&   $a[$k] != $a[$l] 
-#               )
-#            {
-#            }
-#         }
-#      }
-#   }
-#}
 munge( $a[0], $a[1], $a[2], $a[3] ) ;
+munge( $a[0], $a[1], $a[3], $a[2] ) ;
 munge( $a[0], $a[2], $a[3], $a[1] ) ;
+munge( $a[0], $a[2], $a[1], $a[3] ) ;
 munge( $a[0], $a[3], $a[1], $a[2] ) ;
+munge( $a[0], $a[3], $a[2], $a[1] ) ;
 
 munge( $a[1], $a[0], $a[2], $a[3] ) ;
+munge( $a[1], $a[0], $a[3], $a[2] ) ;
 munge( $a[1], $a[2], $a[3], $a[0] ) ;
+munge( $a[1], $a[2], $a[0], $a[3] ) ;
 munge( $a[1], $a[3], $a[0], $a[2] ) ;
+munge( $a[1], $a[3], $a[2], $a[0] ) ;
 
 munge( $a[2], $a[1], $a[0], $a[3] ) ;
+munge( $a[2], $a[1], $a[3], $a[0] ) ;
 munge( $a[2], $a[0], $a[3], $a[1] ) ;
+munge( $a[2], $a[0], $a[1], $a[3] ) ;
 munge( $a[2], $a[3], $a[1], $a[0] ) ;
+munge( $a[2], $a[3], $a[0], $a[1] ) ;
 
 munge( $a[3], $a[1], $a[2], $a[0] ) ;
+munge( $a[3], $a[1], $a[0], $a[2] ) ;
 munge( $a[3], $a[2], $a[0], $a[1] ) ;
+munge( $a[3], $a[2], $a[1], $a[0] ) ;
 munge( $a[3], $a[0], $a[1], $a[2] ) ;
+munge( $a[3], $a[0], $a[2], $a[1] ) ;
 
 exit 0 ;
 
