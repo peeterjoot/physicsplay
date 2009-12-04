97c959efb2b1c960850164e31c2d27099efed94f
peeterj <peeterj>
2009-12-04 02:57:08 +0000

    *** empty log message ***
    Former-commit-id: 1891013e8157733e7b35fa572b676830d3f8639f
    Former-commit-id: 830131d08ede6769955eb065123ccfa67577b921
    Former-commit-id: a53c5b0fe29f2d17b15ed3267cf25a1f333a4725

--- notes/dekker/dekker.c
+++ notes/dekker/dekker.c
@@ -8,59 +8,73 @@
 
 #define N 10000000
 
-volatile int flag[2][/*ignore*/4];
-volatile int turn=0;
+volatile int flag[2][/*ignore*/4] ;
+volatile int turn=0 ;
 
 void my_lock(int tid)
 {
-    flag[tid][/*ignore*/0] = 1;
-    turn = 1-tid;
-    //__asm__ ("mfence"); 
+    flag[tid][/*ignore*/0] = 1 ;
+    turn = 1-tid ;
+
+#if defined USE_MFENCE
+    __asm__ __volatile__( "mfence" ::: "memory" ) ;
+#elif defined USE_SFENCE
+    __asm__ __volatile__( "sfence" ::: "memory" ) ;
+#elif defined USE_LFENCE
+    __asm__ __volatile__( "lfence" ::: "memory" ) ;
+#elif defined USE_SCHED_FENCE
+    __asm__ __volatile__( "" ::: "memory" ) ;
+#else
+    /* no fence */
+#endif
+
     while( flag[1-tid][/*ignore*/0] && turn != tid )
-        ;
+ ;
 }
 
 void my_unlock(int tid)
 {
-    flag[tid][/*ignore*/0] = 0;
+    flag[tid][/*ignore*/0] = 0 ;
 }
 
-unsigned foo=0;
-    
-void *thread_0() 
+unsigned foo=0 ;
+   
+extern "C"
+void *thread_0( void * ) 
 {
-    int i,j;
+    int i,j ;
     for( i=0; i<N; i++ ) {
-        my_lock(0);
-        foo++;
-        my_unlock(0);
+        my_lock(0) ;
+        foo++ ;
+        my_unlock(0) ;
     }
 }
 
-void *thread_1() 
+extern "C"
+void *thread_1( void * ) 
 {
-    int i,j;
+    int i,j ;
     for( i=0; i<N; i++ ) {
-        my_lock(1);
-        foo++;
-        my_unlock(1);
+        my_lock(1) ;
+        foo++ ;
+        my_unlock(1) ;
     }
 }
 
 int main(int argc, char *argv[])
 {
-    pthread_t t0, t1;
-    double t = time(NULL);
+    pthread_t t0, t1 ;
+    double t = time(NULL) ;
 
-    pthread_create(&t0, NULL, thread_0, NULL); 
-    pthread_create(&t1, NULL, thread_1, NULL); 
+    pthread_create(&t0, NULL, thread_0, NULL) ;
+    pthread_create(&t1, NULL, thread_1, NULL) ;
 
-    pthread_join(t0, NULL);
-    pthread_join(t1, NULL);
-    t = time(NULL) - t;
+    pthread_join(t0, NULL) ;
+    pthread_join(t1, NULL) ;
+    t = time(NULL) - t ;
 
-    printf("2*N - foo  = %3u (foo = %u)\n\n", 2*N-foo, foo );
-    printf("time       = %3.0f seconds\n", t );
-    return 0;
+    printf("2*N - foo  = %3u (foo = %u)\n\n", 2*N-foo, foo ) ;
+    printf("time       = %3.0f seconds\n", t ) ;
+    return 0 ;
 }
 
