4ae7b063e4b197f6453b57a6bfef94e5271cf47e
Peeter <Peeter>
2009-12-04 22:17:14 +0000

    *** empty log message ***
    Former-commit-id: b74430b70fe13c04bf02581180d0662f5d8ffaa9
    Former-commit-id: e7a2ff898ace5b16e87d1592f8e8ed661dbda112
    Former-commit-id: df81d7d98fd922fc098638f42e74ea13b19476d4

--- notes/dekker/grabit.sh
+++ notes/dekker/grabit.sh
@@ -1,4 +1,5 @@
-scp peeterj@hotel80:/home/hotel80/peeterj/tor/*.c .
-scp peeterj@hotel80:/home/hotel80/peeterj/tor/Makefile .
-scp peeterj@hotel80:/home/hotel80/peeterj/tor/*.sh     .
-scp peeterj@hotel80:/home/hotel80/peeterj/tor/RCS/* RCS/
+#scp peeterj@hotel80:/home/hotel80/peeterj/tor/*.c .
+#scp peeterj@hotel80:/home/hotel80/peeterj/tor/Makefile .
+#scp peeterj@hotel80:/home/hotel80/peeterj/tor/*.sh     .
+#scp peeterj@hotel80:/home/hotel80/peeterj/tor/RCS/* RCS/
+scp peeterj@hotel80:/home/hotel80/peeterj/tor/out.txt .
