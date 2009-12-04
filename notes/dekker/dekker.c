cae0e04e81f0fd4ab68669ad8d25fde9983252d7
peeterj <peeterj>
2009-12-04 04:23:02 +0000

    *** empty log message ***
    Former-commit-id: 9f0cff42bce0d01f8040cd5514cce1b29148bd61
    Former-commit-id: 03c5254e01685ff69295759f5a6edb3be0180797
    Former-commit-id: d0bb050de03692028cfa41b36abdf1e4c63d8d90

--- notes/dekker/dekker.c
+++ notes/dekker/dekker.c
@@ -8,56 +8,182 @@
 
 #define N 10000000
 
-volatile int flag[2][/*ignore*/4] ;
-volatile int turn=0 ;
+#define MFENCE \
+   __asm__ __volatile__( "mfence" ::: "memory" ) 
 
-void my_lock(int tid)
-{
-    flag[tid][/*ignore*/0] = 1 ;
-    turn = 1-tid ;
+#define SFENCE \
+   __asm__ __volatile__( "sfence" ::: "memory" ) 
+
+#define LFENCE \
+   __asm__ __volatile__( "lfence" ::: "memory" ) 
+
+#define SCHED_FENCE \
+   __asm__ __volatile__( "" ::: "memory" ) 
 
 #if defined USE_MFENCE
-    __asm__ __volatile__( "mfence" ::: "memory" ) ;
+
+   #define FENCE MFENCE
+
 #elif defined USE_SFENCE
-    __asm__ __volatile__( "sfence" ::: "memory" ) ;
+
+   #define FENCE SFENCE
+
 #elif defined USE_LFENCE
-    __asm__ __volatile__( "lfence" ::: "memory" ) ;
+
+   #define FENCE LFENCE
+
 #elif defined USE_SCHED_FENCE
-    __asm__ __volatile__( "" ::: "memory" ) ;
+
+   #define FENCE SCHED_FENCE
+
 #else
-    /* no fence */
+
+   #define FENCE 0 /* no fence */
+
 #endif
 
-    while( flag[1-tid][/*ignore*/0] && turn != tid )
- ;
+struct FlagState
+{
+   volatile int flag ;
+
+   char dummy[120] ; // cacheline separator.
+} ;
+
+FlagState g[2] ;
+volatile int turn = 0 ;
+
+#if defined USE_ATOMIC
+
+volatile char lockWord = 0 ;
+
+void my_lock( int tidIgnored )
+{
+   char oldValue ;
+   char newValue = 1 ;
+
+   do {
+      // 
+      // A dumb spinlock implementation, with no memory barriers.
+      //
+      __asm__ __volatile__( "xchgb  %0,%1\n\t" : "=r" (oldValue), "+m" (lockWord) : "0" (newValue) ) ;
+
+   } while ( 1 == oldValue ) ;
+}
+
+void my_unlock( int tidIgnored )
+{
+   SCHED_FENCE ;
+
+   lockWord = 0 ;
+}
+#elif defined USE_WIKI
+/* 
+ * wikipedia implementation. p0
+ *
+     flag[0] := true
+     while flag[1] = true {
+         if turn . 0 {
+             flag[0] := false
+             while turn . 0 {
+             }
+             flag[0] := true
+         }
+     }
+ 
+    // critical section
+    ...
+    turn := 1
+    flag[0] := false
+ */
+void my_lock( int tid )
+{
+   int other = 1 - tid  ;
+
+   g[tid].flag = true ;
+
+   FENCE ;
+
+   while ( true == g[other].flag )
+   {
+      if ( turn != tid )
+      {
+         g[tid].flag = false ;
+
+         while ( turn != tid )
+         {
+         }
+
+         g[tid].flag = true ;
+      }
+   }
+}
+
+void my_unlock( int tid )
+{
+   int other = 1 - tid  ;
+
+   turn = other ;
+   g[tid].flag = false ;
+}
+#else
+void my_lock(int tid)
+{
+    g[tid].flag = 1 ;
+    turn = 1-tid ;
+
+    FENCE ;
+
+    while( g[1-tid].flag && turn != tid ) ;
 }
 
 void my_unlock(int tid)
 {
-    flag[tid][/*ignore*/0] = 0 ;
+    g[tid].flag = 0 ;
 }
+#endif
 
-unsigned foo=0 ;
-   
-extern "C"
+unsigned foo = 0 ;
+
+#if defined __cplusplus 
+#define EXTERNC extern "C"
+#endif
+
+EXTERNC
 void *thread_0( void * ) 
 {
-    int i,j ;
+    int i ;
+    int p = 0 ;
     for( i=0; i<N; i++ ) {
         my_lock(0) ;
         foo++ ;
         my_unlock(0) ;
+
+        p++ ;
+
+        if ( 0 == (p % 100000) )
+        {
+           printf("0: %d\n", p) ;
+        }
     }
 }
 
-extern "C"
+EXTERNC
 void *thread_1( void * ) 
 {
-    int i,j ;
+    int i ;
+    int p = 0 ;
+
     for( i=0; i<N; i++ ) {
         my_lock(1) ;
         foo++ ;
         my_unlock(1) ;
+
+        p++ ;
+
+        if ( 0 == (p % 100000) )
+        {
+           printf("1: %d\n", p) ;
+        }
     }
 }
 
@@ -77,4 +203,3 @@ int main(int argc, char *argv[])
     printf("time       = %3.0f seconds\n", t ) ;
     return 0 ;
 }
-
