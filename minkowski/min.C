0232dccd1590755f55e5c461d010af772338c37b
Peeter Joot <peeter.joot@gmail.ccom>
2011-02-01 02:25:24 +0100

    new file:   min.C
    Former-commit-id: ea5295d9bfe0677685d008d1b0c90555e90bc30a
    Former-commit-id: 794b0a6c40f9b631f8c586d8157b4b369340cbd3
    Former-commit-id: a462e36150d032cd33b0e02367c8842b42d2aa36

---
+++ minkowski/min.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main( int argc, char * argv[])
+{
+	int c ;
+
+	while ( (c = getopt( argc, argv, ¨v:¨) ) != EOF )
+	{
+		switch ( c )
+		{
+			case 
+		}
+	}
+int getopt(int argc, char * const argv[], const char *optstring);
+
+		
+}
