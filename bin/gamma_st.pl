508d1f9d873f3dfba37170fecf152540a4f280f2
Peeter Joot <peeter.joot@gmail.com>
2012-11-18 00:25:55 -0500

    new file:   ../../bin/gamma_st.pl modified:   modernOpticsMidtermReflection.tex new file:   modernOpticsMidtermReflectionFig5.png
    Former-commit-id: 51d39260787e17da5c2b202435697a879b1590e7
    Former-commit-id: fc12e02fd1a79e47cd39b52446dd88d5f347aa2d
    Former-commit-id: fe2c8c9f400496a9003227cf12e932d97066f22e

---
+++ bin/gamma_st.pl
@@ -0,0 +1,6 @@
+s/\\Gamma_st/\\Gamma_{\\mathrm{st}}/g ;
+s/\\Gamma_{st}/\\Gamma_{\\mathrm{st}}/g  ;
+s/\\Gamma_sp/\\Gamma_{\\mathrm{sp}}/g  ;
+s/\\Gamma_{sp}/\\Gamma_{\\mathrm{sp}}/g  ;
+s/\\Gamma_cav/\\Gamma_{\\mathrm{cav}}/g  ;
+s/\\Gamma_{cav}/\\Gamma_{\\mathrm{cav}}/g  ;
