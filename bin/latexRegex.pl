fad80383c77dbb9ddc4b51635fa3af7d459062fd
Peeter Joot <peeter.joot@gmail.com>
2013-03-06 10:10:48 -0500

    modified:   ../../bin/latexRegex.pl modified:   basicStatMechProblemSet5.tex modified:   basicStatMechProblemSet5Problem3.tex
    Former-commit-id: f0e50c0c6316c542736e0d53512b35e646578604
    Former-commit-id: 2cd6bf96a7179c98edb2a5772e3968bb46b02ad5
    Former-commit-id: 8c534a8667936b68fd9b8b84f8873071e74ec751

--- bin/latexRegex.pl
+++ bin/latexRegex.pl
@@ -1,5 +1,5 @@
 #s/\\Br_av/\\Br_{\\mathrm{av}}/g ;
-#s/\\EE/\\mathcal{E}/g ;
+#s/\\down\b/\\downarrow/g ;
 #s/\\Gamma_{cav}/\\Gamma_{\\mathrm{cav}}/g  ;
 #s/\\Gamma_{sp}/\\Gamma_{\\mathrm{sp}}/g  ;
 #s/\\Gamma_{st}/\\Gamma_{\\mathrm{st}}/g  ;
@@ -8,9 +8,11 @@
 #s/\\Gamma_st/\\Gamma_{\\mathrm{st}}/g ;
 #s/\\PP/\\mathcal{P}/g ;
 #s/\\rcap_av/\\rcap_{\\mathrm{av}}/g ;
+#s/\\Shat/\\hat{S}/g ;
 #s/\\spacegrad_T/\\spacegrad_{\\mathrm{T}}/g ;
 #s/\\tp/\\tilde{p}/g ;
 #s/\\tx/\\tilde{x}/g ;
+#s/\\up\b/\\uparrow/g ;
 #s/\^classical/^{\\mathrm{classical}}/g ;
 #s/\bialpha\b/{i_\\alpha}/g ;
 #s/\tY\b/\\tilde{Y}/g ;
@@ -36,8 +38,10 @@
 #s/_quantum/_{\\mathrm{quantum}}/g ;
 #s/_subsystem/_{\\text{subsystem}}/g ;
 #s/_T/_{\\mathrm{T}}/g ;
+#s/_total/_{\\mathrm{total}}/g ;
 #s/_V/_{\\mathrm{V}}/g ;
 #s/ENV/{(\\mathrm{E}, \\mathrm{N}, \mathrm{V})}/g ;
+#s/k_B/k_{\\mathrm{B}}/g ;
 #s/N_c/N_{\\mathrm{c}}/g ;
 #s/n1/n_1/g ;
 #s/n2/n_2/g ;
@@ -50,8 +54,4 @@
 #s/pidotalpha/\\dot{p}_{i_\\alpha}/g ;
 #s/xialpha\b/x_{i_\\alpha}/g ;
 #s/xidotalpha/\\dot{x}_{i_\\alpha}/g ;
-s/\\Shat/\\hat{S}/g ;
-s/\\down\b/\\downarrow/g ;
-s/\\up\b/\\uparrow/g ;
-s/_total/_{\\mathrm{total}}/g ;
-s/k_B/k_{\\mathrm{B}}/g ;
+s/\\EE/\\mathcal{E}/g ;
