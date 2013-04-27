92194a30e18c239a403424b8e0eae2b5d07ad704
Joot <peeterj@hotellnx94.torolab.ibm.com>
2013-04-26 23:51:46 -0400

    modified:   makefile modified:   makefile.hotellnx94 modified:   regexMatch.cpp
    Former-commit-id: 4df0ac7720a4f59b13605f05a3196c82a483e50e
    Former-commit-id: 2eb895d17a169b8dc410f14373ae5cd8864c7cb6
    Former-commit-id: a7dfc5cf0ede5bed84c254ae2cdc2f0382bb939b

--- regex/regexMatch.cpp
+++ regex/regexMatch.cpp
@@ -4,13 +4,13 @@
 
 int main()
 {
-   boost::regex reg( "(.*) (.*)", boost::regiex::perl ) ;
+   boost::regex reg( "(.*) (.*)", boost::regex::perl ) ;
 
    std::string s = "a b" ;
 
-   s = boost::regex_replace( s, reg, "$2$1" ) ;
+   s = boost::regex_replace( s, reg, "$2 $1" ) ;
 
-   std::cout << s << endl ;
+   std::cout << s << std::endl ;
 
    return 0 ;
 }
