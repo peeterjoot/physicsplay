19a5fa46ed90f88ec603f1b47cb6e12504baa748
Peeter Joot <peeter.joot@gmail.com>
2013-04-26 23:27:40 -0400

    modified:   regexMatch.cpp
    Former-commit-id: d36de359dd2b36fd0370e92e4938a829dd26b31c
    Former-commit-id: 73365a743a8c2a0cf010a8d264ee821466357e06
    Former-commit-id: fa733a44ffd59acd37e23be74003334fbca10495

--- regex/regexMatch.cpp
+++ regex/regexMatch.cpp
@@ -6,4 +6,11 @@ int main()
 {
    boost::regex reg( "(.*) (.*)", boost::regiex::perl ) ;
 
+   std::string s = "a b" ;
 
+   s = boost::regex_replace( s, reg, "$2$1" ) ;
+
+   std::cout << s << endl ;
+
+   return 0 ;
+}
