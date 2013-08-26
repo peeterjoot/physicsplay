b723c993f10901d1a6e4a4c6edee78ea8da8cadf
Peeter Joot <peeter.joot@gmail.com>
2013-08-25 23:56:04 -0400

    modified:   blogSearchSummary.pl
    Former-commit-id: 54a9d7bc3c08c95c79aca57d5be5a8eb2f65559f
    Former-commit-id: 1d19f74e9e23940e2e889c6ca244b81710da2975
    Former-commit-id: 517406421d563aa0a7bb084a832e626140f166b6

--- bin/blogSearchSummary.pl
+++ bin/blogSearchSummary.pl
@@ -2,96 +2,111 @@
 
 my @keys = (
    [
-      'just\s*energy|ontario energy'
-# blog only
+      'just\s*energy|ontario energy',
+      'http://peeterjoot.wordpress.com/tag/just-energy-canada'
    ],
    [
-      'phy356|liboff|lowering|square well|commutator|pauli|raising|generator'
-#https://sites.google.com/site/peeterjoot/math2010/phy356.pdf
+      'phy356|liboff|lowering|square well|commutator|pauli|raising|generator',
+      'https://sites.google.com/site/peeterjoot/math2010/phy356.pdf'
    ],
    [
-      'desai'
-#Believed to be typos in Desai's QM Text
-#http://sites.google.com/site/peeterjoot/math2010/desaiTypos.pdf
-   ],
-   [
-      'phy456|fermi golden rule'
-#https://sites.google.com/site/peeterjoot/math2011/phy456.pdf
-   ],
-   [
-      'quantum|dirac'
-   ],
-   [
-      'phy450|relativistic|poppitz|electrodynamics|four velocity|boost|lorentz force|retarded time|lorentz|lienard.*wiechert'
-#https://sites.google.com/site/peeterjoot/math2011/phy450.pdf
+      'desai',
+      'http://sites.google.com/site/peeterjoot/math2010/desaiTypos.pdf'
    ],
 #   [
-#      'arxiv'
-#Change of basis and Gram-Schmidt orthonormalization in special relativity
-# http://arxiv.org/abs/1104.4829
+#      'phy456|fermi golden rule',
+#      'https://sites.google.com/site/peeterjoot/math2011/phy456.pdf'
 #   ],
-#Relativistic origins of the Schrodinger equation
    [
-      'phy454|continuum|lame'
-#https://sites.google.com/site/peeterjoot2/math2012/phy454.pdf
+      'quantum|dirac',
+      'https://sites.google.com/site/peeterjoot/math2011/phy356.pdf',
+      'https://sites.google.com/site/peeterjoot/math2011/phy456.pdf'
    ],
    [
-      'banister|bannister|newel'
-# blog only
+      'phy450|relativistic|poppitz|electrodynamics|four velocity|boost|lorentz force|retarded time|lorentz|lienard.*wiechert',
+      'https://sites.google.com/site/peeterjoot/math2011/phy450.pdf',
+      'https://sites.google.com/site/peeterjoot2/math2013/classicalmechanics.pdf'
+   ],
+#   [
+#      'arxiv',
+#      'http://arxiv.org/abs/1104.4829'
+#   ],
+   [
+      'phy454|continuum|lame',
+      'https://sites.google.com/site/peeterjoot2/math2012/phy454.pdf'
    ],
    [
-      'phy354|noether|lagrangian|hamiltonian|action|conjugate momenta|routh|classical mechanics|pendulum|canonical'
-# not posted
+      'banister|bannister|newel',
+      'http://peeterjoot.wordpress.com/2010/06/06/fixing-a-loose-banister',
+      'http://peeterjoot.wordpress.com/category/home-improvement'
    ],
    [
-      'ud2a'
-# blog only
+      'phy354|noether|lagrangian|hamiltonian|action|conjugate momenta|routh|classical mechanics|pendulum|canonical',
+      'https://sites.google.com/site/peeterjoot2/math2013/classicalmechanics.pdf'
    ],
    [
-      'root|8200'
-# blog only
+      'ud2a',
+      'http://peeterjoot.wordpress.com/2010/05/26/a-fun-and-curious-dig-gcc-generation-of-a-ud2a-instruction-sigill/'
    ],
    [
-      'fence|sync|barrier|atomic|lwarx|stwcx'
+      'root|8200',
+      'http://peeterjoot.wordpress.com/2009/10/02/how-to-get-a-root-shell-if-you-can-modify-code-that-runs-setuid-root',
+      'http://peeterjoot.wordpress.com/2009/12/07/software-developer-spotted-on-8200-warden-property/'
+   ],
+   [
+      'fence|sync|barrier|atomic|lwarx|stwcx',
 #      An attempt to illustrate differences between memory ordering and atomic access
-#      , http://sites.google.com/site/peeterjoot/math2009/atomic.pdf
+      'http://sites.google.com/site/peeterjoot/math2009/atomic.pdf',
 #      A nice simple example of a memory barrier requirement
-#      , https://sites.google.com/site/peeterjoot/math2010/atomicSimple.pdf
+      'https://sites.google.com/site/peeterjoot/math2010/atomicSimple.pdf',
 #      Timings and correctness issues for mutex release operations
-#      http://sites.google.com/site/peeterjoot/math2010/mutexRelease.pdf      
+      'http://sites.google.com/site/peeterjoot/math2010/mutexRelease.pdf'
    ],
    [
-      'git'
-# blog only
+      'git',
+      'http://peeterjoot.wordpress.com/2009/12/16/abusing-the-git-version-control-system-as-a-distributed-filesystem'
    ],
    [
-      'gdb|scheduler-locking|dbx'
-# blog only
+      'gdb|scheduler-locking|dbx',
+      'http://peeterjoot.wordpress.com/category/debugging/'
    ],
    [
-      'geometric|wedge|clifford|bivector'
-# https://sites.google.com/site/peeterjoot/math2009/gabook.pdf
+      'geometric|wedge|clifford|bivector',
+      'https://sites.google.com/site/peeterjoot/math2009/gabook.pdf'
    ],
    [
-      'align|padding'
-# blog only
+      'align|padding',
+      'http://peeterjoot.wordpress.com/2009/11/11/c-structure-alignment-padding/'
    ],
    [
-      'nm|bsymbolic'
-# blog only
+      'nm|bsymbolic',
+      'http://peeterjoot.wordpress.com/2010/03/22/using-nm-to-track-down-the-origin-of-an-undefined-symbol/'
    ],
    [
-      'alias'
-# blog only
+      'alias',
+      'http://peeterjoot.wordpress.com/2010/07/09/use-of-unions-to-deal-with-aliasing-problems/',
+      'http://peeterjoot.wordpress.com/2009/11/14/c-pointer-aliasing-violations-and-aggressive-compiler-optimizations/'
    ],
    [
-      'xargs|perl|awk'
-# blog only
+      'xargs',
+      'http://peeterjoot.wordpress.com/2010/02/28/shell-tips-and-tricks-the-most-basic-stuff/'
    ],
    [
-      'joot|peeter|peter'
-   ]
-   #   ,'phy485'
+      'perl',
+      'http://peeterjoot.wordpress.com/category/perl-and-general-scripting-hackery/'
+   ],
+#   [
+#      'awk',
+#      'http://peeterjoot.wordpress.com/2009/08/19/doing-awk-like-stuff-in-perl/'
+#   ],
+#   [
+#      'phy485',
+#      'https://sites.google.com/site/peeterjoot2/math2012/phy485.pdf'
+#   ],
+   [
+      'joot|peeter|peter',
+      ''
+   ],
 ) ;
 
 my $first = 1 ;
@@ -124,6 +139,7 @@ foreach my $kk ( @keys )
    my $n = 0 ;
 
    my $k = $kk->[0] ;
+   my @r = splice( @$kk, 1, scalar(@$kk) ) ;
 
    foreach ( sort { $counts{$b} <=> $counts{$a} } keys %counts )
    {
@@ -141,7 +157,9 @@ foreach my $kk ( @keys )
       }
    }
 
-   $out{$k} = "$k: $n\n$s\n\n" ;
+   my $rr = join( "\n", @r ) ;
+
+   $out{$k} = "$k: $n\n$s$rr\n\n" ;
    $outcount{$k} = $n ;
 }
 
