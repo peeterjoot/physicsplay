208ea00208b4a37963a7e056b7cffbfda76541dd
Peeter Joot <peeter.joot@gmail.com>
2013-10-25 09:30:07 -0400

    modified:   runsamples.sh
    Former-commit-id: 6c6d4e5bffdf577aa5824e85af07e5b66048d362
    Former-commit-id: 3559f54d432d448c97e31749aef9782379c695f0
    Former-commit-id: 421bbbe73e887c3f9eac9f5beba95bb2fbe097da

--- del2xml/runsamples.sh
+++ del2xml/runsamples.sh
@@ -1,3 +1,8 @@
 ./del2xml < sample.csv > sample.xml
 ./del2xml -dollar < sample2.csv > sample2.xml
 ./del2xml -del Z < sample3.csv > sample3.xml
+
+diff *2.xml *3.xml > all.diff
+diff sample.xml *3.xml >> all.diff
+echo 'test.  wc should be zero'
+wc -l all.diff
