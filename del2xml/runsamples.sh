e90b7ea5889b73403a0ce5f7c591746e403cff7a
Peeter Joot <peeter.joot@gmail.com>
2013-10-25 21:14:19 -0400

    new file:   .junk/r modified:   del2xml modified:   runsamples.sh modified:   sample.csv deleted:    sample2.csv deleted:    sample3.csv
    Former-commit-id: df5e3cc612dd1353dc4ebd727d95853b5d951c72
    Former-commit-id: 1983612a6610edffa0686419078ec744beaf3e49
    Former-commit-id: 92cf507f69d04361cd4a7939007a28b66201dd3d

--- del2xml/runsamples.sh
+++ del2xml/runsamples.sh
@@ -1,3 +1,6 @@
+sed 's/ *, */$/g' < sample.csv > sample2.csv
+sed 's/ *, */Z/g' < sample.csv > sample3.csv
+
 ./del2xml < sample.csv > sample.xml
 ./del2xml -dollar < sample2.csv > sample2.xml
 ./del2xml -del Z < sample3.csv > sample3.xml
