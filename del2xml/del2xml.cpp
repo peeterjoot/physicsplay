67d5b834e4acbf2d2f590fb47fd4144088271eb4
Peeter Joot <peeter.joot@gmail.com>
2013-10-28 22:17:45 -0400

    new file:   Makefile modified:   del2xml.cpp
    Former-commit-id: 6dd920b8c6c64460b45887c77841bdaea3106164
    Former-commit-id: 73e18cac3c5ef926aa48102f6f32197dd096a9e4
    Former-commit-id: d8e2c69a60357b7ce6ba5dcdf417ee81e9ab2491

--- del2xml/del2xml.cpp
+++ del2xml/del2xml.cpp
@@ -3,15 +3,14 @@
 #include <cassert>
 #include <cstring>
 #include <vector>
+#include <sstream>
+#include <cstdlib>
+#include <iostream>
 
 using namespace std ;
 
 /** represents either a set of column tags from the first line, or the column data for a given row */
-struct columnData
-{
-   /** one column tag or value per entry */
-   vector<string> c ;
-} ;
+typedef vector<string> columnData ;
 
 /**
    As we navigate over the rows, collect stats on what we find on the columns so that we can pick an SQL type later.
@@ -35,12 +34,12 @@ struct columnTypeInfo
 
 bool                    g_bPrintHelpAndExit ;
 double                  g_varcharFraction = 0.5 ;
-vector<columnData>      g_headers ;
+columnData              g_headers ;
 vector<columnData>      g_rows ;
 vector<columnTypeInfo>  g_typeInfo ;
 int                     g_numTags = 0 ;
 int                     g_rowIter = 0 ;
-string                  g_delimiter = "," ; // FIXME: hardcoded.
+char                    g_delimiter = ',' ; // FIXME: hardcoded.
 //string                  g_deltext ;
 //string                  g_dollar ;
 
@@ -48,6 +47,7 @@ static void showHelpAndExit() ;
 static void parseArguments( int argc, char ** argv ) ;
 static void printOneDataRow( int rowIndex ) ;
 static void printOneColumnMetaData( int c ) ;
+static void consumeOneLine( const string & line ) ;
 
 int main( int argc, char ** argv )
 {
@@ -58,46 +58,41 @@ int main( int argc, char ** argv )
       showHelpAndExit() ;
    }
 
-   printf( "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-<DataSet xmlns=\"http://www.ibm.com/qmf\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">
+   printf( "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+           "<DataSet xmlns=\"http://www.ibm.com/qmf\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n"
+           "\n"
+           "  <ResultSet>\n"
+           "    <MetaData>\n"
+           "      <SourceDescription/>\n" ) ;
 
-  <ResultSet>
-    <MetaData>
-      <SourceDescription/>
-" ) ;
+   string s ;
 
-#if 0
-   while ( <> )
+   while ( getline( cin, s ) )
    {
-      consumeOneLine( $_ ) ;
+      consumeOneLine( s ) ;
    }
-#endif
 
-   print "      <ColumnsAmount>$g_numTags</ColumnsAmount>\n" ;
+   printf( "      <ColumnsAmount>%d</ColumnsAmount>\n", g_numTags ) ;
 
    for ( int c = 0 ; c < g_numTags ; c++ )
    {
       printOneColumnMetaData( c ) ;
    }
 
-   printf("
-    </MetaData>
-    <Data>
-" ) ;
+   printf( "    </MetaData>\n"
+           "    <Data>\n" ) ;
 
    for ( int r = 0 ; r < g_rowIter ; r++ )
    {
       printOneDataRow( r ) ;
    }
 
-printf( "
-     </Data>
-  </ResultSet>
-
-  <Extensions/>
-
-</DataSet>
-" ) ;
+   printf( "     </Data>\n"
+           "  </ResultSet>\n"
+           "\n"
+           "  <Extensions/>\n"
+           "\n"
+           "</DataSet>\n" ) ;
 
    return 0 ;
 }
@@ -105,18 +100,18 @@ printf( "
 void parseArguments( int argc, char ** argv )
 {
 #if 0 // FIXME: hardcoded.
-if ( defined $g_dollar )
-{
-   $g_delimiter = qr(\$) ;
-}
-elsif ( defined $g_deltext )
-{
-   $g_delimiter = qr($g_deltext) ;
-}
-else
-{
-   $g_delimiter = qr(\s*,\s*) ; // opt-spaces comma opt-spaces
-}
+   if ( defined g_dollar )
+   {
+      g_delimiter = qr(\$) ;
+   }
+   elsif ( defined g_deltext )
+   {
+      g_delimiter = qr(g_deltext) ;
+   }
+   else
+   {
+      g_delimiter = qr(\s*,\s*) ; // opt-spaces comma opt-spaces
+   }
 #endif
 }
 
@@ -125,69 +120,74 @@ void showHelpAndExit()
    printf( "del2xml < input > output\n" ) ;
 
 #if 0
-   printf "del2xml [-help] [-delimiter blah] [-dollar] [-varfrac vcFraction]
-
-Options:
-\t-dollar            \tUse '$' as a delimiter.
-\t-delimiter blah    \tUse 'blah' as a delimiter.
-\t-varfrac vcFraction\tIf the size of the storage used in char representation times vcFraction
-\t                   \tis greater than the space required for the same data in varchar
-\t                   \tformat, use a varchar type instead of char.  vcFraction defaults to 0.5
-
-Default delimiter is '[optional-spaces],[optional-spaces]'\n" ) ;
+   printf( "del2xml [-help] [-delimiter blah] [-dollar] [-varfrac vcFraction]\n"
+           "\n"
+           "Options:\n"
+           "\t-dollar            \tUse '$' as a delimiter.\n"
+           "\t-delimiter blah    \tUse 'blah' as a delimiter.\n"
+           "\t-varfrac vcFraction\tIf the size of the storage used in char representation times vcFraction\n"
+           "\t                   \tis greater than the space required for the same data in varchar\n"
+           "\t                   \tformat, use a varchar type instead of char.  vcFraction defaults to 0.5\n"
+           "\n"
+           "Default delimiter is '[optional-spaces],[optional-spaces]'\n" ) ;
 #endif
 
    exit( 1 ) ;
 }
 
-void split( vector<string> & e, string del, const char * const line )
+static void split( vector<string> & e, char del, const string & line )
 {
-   assert( 0 ) ;
+   stringstream ss( line ) ;
+   string s;
 
-// split by delimiter.  What's the natural way to do this in C++?
-//   vector<string> e = split( g_delimiter, line ) ;
+   while ( getline( ss, s, del ) )
+   {
+      e.push_back( s ) ;
+   }
 }
 
-void consumeOneLine( char * line )
+void consumeOneLine( const string & line )
 {
+   vector<string> e ;
+
    split( e, g_delimiter, line ) ;
 
-   int numColumns = e.len( ) ; 
+   int numColumns = e.size( ) ; 
 
    if ( g_numTags )
    {
-      assert( numColumns == numTags ) ;
+      assert( numColumns == g_numTags ) ;
 
-      g_rows[g_rowIter].c = e ;
+      g_rows.push_back( e ) ;
 
-      for ( int i = 0 ; i < e.len() ; i++ )
+      for ( int i = 0 ; i < numColumns ; i++ )
       {
          string v = e[ i ] ;
          int l = v.length( ) ;
 
          // save for average/stddev len calculation if char (to decide for char vs varchar).
-         g_typeInfo[i].charLen += l ;
+         g_typeInfo[ i ].charLen += l ;
 
-         if ( g_typeInfo[i].sizes < l )
+         if ( g_typeInfo[ i ].sizes < l )
          {
-            g_typeInfo[i].sizes = l ;
+            g_typeInfo[ i ].sizes = l ;
          }
 
          bool foundNonChar = 0 ;
 
          char * err = 0 ;
-         strtod( v.c_str(), &err, 10 ) ;
+         strtol( v.c_str(), &err, 10 ) ;
 
          if ( !err[0] )
          {
-            g_typeInfo[i].totalIntegersInColumn++ ;
+            g_typeInfo[ i ].totalIntegersInColumn++ ;
 
             foundNonChar = 1 ;
          }
 
 //         if ( v =~ /^(\d+)\.(\d+)/ )
 //         {
-//               g_decimal[i]++ ;
+//               g_decimal[ i ]++ ;
 //
 //               foundNonChar = 1 ;
 //         }
@@ -195,7 +195,7 @@ void consumeOneLine( char * line )
 #if 0
          if ( v =~ qr(^\d\d/\d\d/\d\d\d\d$) )
          {
-            g_typeInfo[i].totalDateFormatsInColumn++ ;
+            g_typeInfo[ i ].totalDateFormatsInColumn++ ;
 
             foundNonChar = 1 ;
          }
@@ -213,77 +213,89 @@ void consumeOneLine( char * line )
    }
    else
    {
-      g_headers.c = e ;
+      g_headers = e ;
       g_numTags = numColumns ;
+
+      g_typeInfo.resize( numColumns ) ;
    }
 }
 
 void printOneColumnMetaData( int c )
 {
    int            cpp   = c + 1 ;
-   vector<string> n     = g_headers[ c ].c ;
-   int            s     = g_sizes[ c ] ;
+   const char *   n     = g_headers[ c ].c_str() ;
+   int            s     = g_typeInfo[ c ].sizes ;
    char           attr[ 256 ] ;
 
-   if ( g_integer[c] == g_rowIter )
+   if ( g_typeInfo[ c ].totalIntegersInColumn == g_rowIter )
    {
-      snprintf( attr, sizeof(attr), "<Type>integer</Type>\n<Width>%d</Width>", s ) ;
+      snprintf( attr, sizeof(attr),
+                "<Type>integer</Type>\n"
+                "        <Width>%d</Width>", s ) ;
    }
 #if 0
-   elsif ( g_decimal[c] == g_rowIter )
+   elsif ( g_decimal[ c ] == g_rowIter )
    {
       // FIXME: http://stackoverflow.com/questions/2377174/how-do-i-interpret-precision-and-scale-of-a-number-in-a-database
 
       // on decimal detection, will need to record the max scale and max prec (and distinguish from integer since valid ints are also 
       // valid decimal)
-      snprintf( attr, sizeof(attr), "<Type>decimal</Type>\n"<Scale>0</Scale>\n<Precision>%d</Precision>", s ) ;
+      snprintf( attr, sizeof(attr),
+                "<Type>decimal</Type>\n"
+                "        <Scale>0</Scale>\n"
+                "        <Precision>%d</Precision>", s ) ;
    }
-   elsif ( g_dateformat[c] == g_rowIter )
+   elsif ( g_dateformat[ c ] == g_rowIter )
    {
       // FIXME: what attributes does date require?  May not have parsed in the correct format.  Don't enable without info.
-      snprintf( attr, sizeof(attr), "<Type>date</Type>\n<Width>%d</Width>", s ) ;
+      snprintf( attr, sizeof(attr), 
+                "<Type>date</Type>\n"
+                "        <Width>%d</Width>", s ) ;
    }
 #endif
    else
    {
-      int varcharSpace = g_typeInfo[c].totalIntegersInColumn++ ;
+      int varcharSpace = g_typeInfo[ c ].totalIntegersInColumn++ ;
       int charSpace = s * g_rowIter ;
 
       // use varchar if it gets us at least a (by default) 50% savings in total space for the table.
       if ( varcharSpace < (charSpace * g_varcharFraction) )
       {
-         snprintf( attr, sizeof(attr), "<Type>varchar</Type>\n<Width>%d</Width>", s ) ;
+         snprintf( attr, sizeof(attr),
+                   "<Type>varchar</Type>\n"
+                   "        <Width>%d</Width>", s ) ;
       }
       else
       {
-         snprintf( attr, sizeof(attr), "<Type>char</Type>\n<Width>%d</Width>", s ) ;
+         snprintf( attr, sizeof(attr), 
+                   "<Type>char</Type>\n"
+                   "        <Width>%d</Width>", s ) ;
       }
    }
 
-   printf( 
-"      <ColumnDescription id=\"%d\">
-        <Name>%s</Name>
-        <Label>%s</Label>
-        %s
-        <Nullable>false</Nullable>
-        <Format>plain</Format>
-      </ColumnDescription>\n", cpp, n, n, attr ) ;
+   printf( "      <ColumnDescription id=\"%d\">\n"
+           "        <Name>%s</Name>\n"
+           "        <Label>%s</Label>\n"
+           "        %s\n"
+           "        <Nullable>false</Nullable>\n"
+           "        <Format>plain</Format>\n"
+           "      </ColumnDescription>\n", cpp, n, n, attr ) ;
 }
 
 void printOneDataRow( int rowIndex )
 {
-   vector<string> c = g_rows[ rowIndex ].c ;
+   columnData & c = g_rows[ rowIndex ] ;
 
    printf( "      <Row id=\"%d\">\n", rowIndex ) ;
 
    for ( int i = 0 ; i < g_numTags ; i++ )
    {
       int cpp = i + 1 ;
-      int sz = g_typeInfo[i].sizes ;
-      char * cv = cr[i].c_str() ;
+      int sz = g_typeInfo[ i ].sizes ;
+      const char * cv = c[ i ].c_str() ;
 
       // FIXME: should varchar output be trailing space padded?  Integer and decimal probably shouldn't either.
-      printf ("        <Cell id=%d>"%-*s"</Cell>\n", cpp, sz, cv ) ;
+      printf( "        <Cell id=\"%d\">%-*s</Cell>\n", cpp, sz, cv ) ;
    }
 
    printf( "      </Row>\n" ) ;
