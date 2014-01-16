b83131ca95e165494beb457e86cae6d629cfebeb
Peeter Joot <peeter.joot@gmail.com>
2014-01-16 04:57:35 -0500

    modified:   history2.sh new file:   toclean.txt
    Former-commit-id: af1eabcc6b38f6c74b9006711f3f00103b8b191a

--- gitcleaning/history2.sh
+++ gitcleaning/history2.sh
@@ -17,3 +17,12 @@ rm allfileshas.txt uniq.txt bigtosmall.txt l q
 cd gitcleaning
 
 cut -f3 -d' ' bigtosmall.missing.txt > bigtosmall.missing.txt.f
+
+grep -F -e .PNG -e .eps -e .png -e .pdf -e .tif -e figures -e .nb -e .cdf bigtosmall.missing.txt > toclean.txt
+grep -vF -e .PNG -e .eps -e .png -e .pdf -e .tif -e figures -e .nb -e .cdf bigtosmall.missing.txt  | less
+
+j=0 ; for i in `cut -f2 -d' ' toclean.txt` ; do j=$(($i + $j)) ; done ; echo $j
+
+24836162
+
+(~23 Mb) worth of stuff still left that is cleanable.  Think that's about 1/3 of the current size of the repo.
