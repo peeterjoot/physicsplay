e7c0385de8a0e03ab2d7fb712f9f0541d7af6b57
Peeter Joot <peeter.joot@gmail.com>
2015-02-18 16:53:18 -0500

    new file:   reciprocityTheorem.tex

--- bin/latexRegex.pl
+++ bin/latexRegex.pl
@@ -133,6 +133,8 @@
 #s/pidotalpha/\\dot{p}_{i_\\alpha}/g ;
 #s/xialpha\b/x_{i_\\alpha}/g ;
 #s/xidotalpha/\\dot{x}_{i_\\alpha}/g ;
-s/\\aut/{\\textrm{AUT}}/g;
-s/\\sg/{\\textrm{sg}}/g;
-s/\\profE/Prof. Eleftheriades/g;
+#s/\\aut/{\\textrm{AUT}}/g;
+#s/\\sg/{\\textrm{sg}}/g;
+#s/\\profE/Prof. Eleftheriades/g;
+s/\^a/^{(a)}/g;
+s/\^b/^{(b)}/g;
