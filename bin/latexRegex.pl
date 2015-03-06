e7c5158d449a17ab1e61ce298c04d9927a3e5df2
Peeter Joot <peeter.joot@gmail.com>
2015-03-06 00:04:24 -0500

    modified:   advancedantennaProblemSet3Problem3.tex new file:   resolvingFieldsIncidentOnPlane.tex

--- bin/latexRegex.pl
+++ bin/latexRegex.pl
@@ -140,3 +140,6 @@
 #s/pidotalpha/\\dot{p}_{i_\\alpha}/g ;
 #s/xialpha\b/x_{i_\\alpha}/g ;
 #s/xidotalpha/\\dot{x}_{i_\\alpha}/g ;
+s/\\LOS/{\\textrm{LOS}}/g;
+s/\\yz/{\\textrm{yz}}/g;
+s/\\zx/{\\textrm{zx}}/g;
