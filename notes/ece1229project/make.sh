3c158207fe5d6e19866066075cdfec922ea64d0f
Peeter Joot <peeter.joot@gmail.com>
2015-04-05 00:15:28 -0400

    modified:   ../METADATA new file:   make.sh new file:   makefile renamed:    ../blogit/presentation.tex -> presentation.tex modified:   ../make_pdflatex

---
+++ notes/ece1229project/make.sh
@@ -0,0 +1,6 @@
+pdflatex -file-line-error -synctex=1 --max-print-line=1000 --c-style-errors --halt-on-error presentation.tex
+bibtex presentation
+pdflatex -file-line-error -synctex=1 --max-print-line=1000 --c-style-errors --halt-on-error presentation.tex
+bibtex presentation
+pdflatex -file-line-error -synctex=1 --max-print-line=1000 --c-style-errors --halt-on-error presentation.tex
+bibtex presentation
