4b3c22eff63ae9a68d179d7cfc122c341e9c74f7
Peeter Joot <peeter.joot@gmail.com>
2015-08-13 10:38:51 -0400

    modified:   makefile new file:   makefile.conspiracy modified:   regexMatch.cpp

--- regex/regexMatch.cpp
+++ regex/regexMatch.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
+#if 0
 #include "boost/regex.hpp"
+#else
+#include <regex>
+#endif
 
 int main()
 {
+   std::string s { "a b" } ;
+
+#if 0
    boost::regex reg( "(.*) (.*)", boost::regex::perl ) ;
 
-   std::string s = "a b" ;
-
    s = boost::regex_replace( s, reg, "$2 $1" ) ;
+#else
+   std::regex reg{ "(.*) (.*)" } ;
+
+   s = std::regex_replace( s, reg, "$2 $1" ) ;
+#endif
 
    std::cout << s << std::endl ;
 
