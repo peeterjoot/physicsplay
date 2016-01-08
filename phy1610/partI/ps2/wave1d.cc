1c9f88cdeffd12ee145de52b8603c55832ed9118
Peeter Joot <peeterjoot@protonmail.com>
2016-01-07 18:06:47 -0800

    modified:   wave1d.cc
    move derived parameters into parameters class.  undo make_unique changes: they don't work with an array that has variable size (vector would be needed).
    What's the cost of the vector class compared to the C array used in this code?

--- phy1610/partI/ps2/wave1d.cc
+++ phy1610/partI/ps2/wave1d.cc
@@ -53,90 +53,91 @@ struct output
    }
 } ;
 
+/// Derived parameters and parsing
+struct parameters : public physical,simulation,output
+{
+   int    ngrid   = (x2-x1)/dx;  // number of x points
+   int    npnts   = ngrid + 2;   // number of x points including boundary points
+   double dt      = 0.5*dx/c;   // time step size
+   int    nsteps  = runtime/dt;  // number of steps of that size to reach s.runtime
+   int    nper    = outtime/dt;  // how many step s between snapshots
+
+   parameters( int argc, char* argv[] )
+   {
+      // Read the values from a file if specified on the command line
+      if (argc > 1) {
+         std::ifstream infile(argv[1]);
+         infile >> c;
+         infile >> tau;
+         infile >> x1;
+         infile >> x2;
+         infile >> runtime;
+         infile >> dx;
+         infile >> outtime;
+         infile.close();
+      }
+
+      ngrid   = (x2-x1)/dx;  // number of x points
+      npnts   = ngrid + 2;   // number of x points including boundary points
+      dt      = 0.5*dx/c;    // time step size
+      nsteps  = runtime/dt;  // number of steps of that size to reach s.runtime
+      nper    = outtime/dt;  // how many step s between snapshots
+
+      // Report all the values
+      std::cout << "#c      " << c      << std::endl;
+      std::cout << "#tau    " << tau    << std::endl;
+      std::cout << "#x1     " << x1     << std::endl;
+      std::cout << "#x2     " << x2     << std::endl;
+      std::cout << "#runtime " << runtime << std::endl;
+      std::cout << "#dx     " << dx     << std::endl;
+      std::cout << "#outtime " << outtime << std::endl; 
+      std::cout << "#ngrid   " << ngrid   << std::endl;
+      std::cout << "#dt     " << dt     << std::endl;
+      std::cout << "#nsteps  " << nsteps  << std::endl;   
+      std::cout << "#nper   " << nper   << std::endl;
+   }
+} ;
+
 int main(int argc, char* argv[])
 {
-   physical p ;
-   simulation s ;
-   output o ;
-
-   // Read the values from a file if specified on the command line
-   if (argc > 1) {
-      std::ifstream infile(argv[1]);
-      infile >> p.c;
-      infile >> p.tau;
-      infile >> p.x1;
-      infile >> p.x2;
-      infile >> s.runtime;
-      infile >> s.dx;
-      infile >> o.outtime;
-      infile.close();
-   }
-
-   // Derived parameters
-   int    ngrid   = (p.x2-p.x1)/s.dx;  // number of x points
-   int    npnts   = ngrid + 2;   // number of x points including boundary points
-   double  dt     = 0.5*s.dx/p.c;   // time step size
-   int    nsteps  = s.runtime/dt;  // number of steps of that size to reach s.runtime
-   int    nper   = o.outtime/dt;  // how many step s between snapshots
-
-   // Report all the values
-   std::cout << "#c      " << p.c      << std::endl;
-   std::cout << "#tau    " << p.tau    << std::endl;
-   std::cout << "#x1     " << p.x1     << std::endl;
-   std::cout << "#x2     " << p.x2     << std::endl;
-   std::cout << "#runtime " << s.runtime << std::endl;
-   std::cout << "#dx     " << s.dx     << std::endl;
-   std::cout << "#outtime " << o.outtime << std::endl; 
-   std::cout << "#ngrid   " << ngrid   << std::endl;
-   std::cout << "#dt     " << dt     << std::endl;
-   std::cout << "#nsteps  " << nsteps  << std::endl;   
-   std::cout << "#nper   " << nper   << std::endl;
-
-   using values = std::array<double, npnts> ;
-   using valuePointer = std::unique_ptr< values > ;
-
-   // Define and allocate arrays, deallocated when the pointers go out of scope.
-   valuePointer rho_prev = std::make_unique< values >() ;
-   valuePointer rho      = std::make_unique< values >() ;
-   valuePointer rho_next = std::make_unique< values >() ;
-   valuePointer x        = std::make_unique< values >() ;
-
-//   double* rho_prev = new double[npnts]; // time step t-1
-//   double* rho     = new double[npnts]; // time step t
-//   double* rho_next = new double[npnts]; // time step t+1
-//   double* x      = new double[npnts]; // x values
+   parameters p( argc, argv ) ;
+   
+   double* rho_prev = new double[p.npnts]; // time step t-1
+   double* rho     = new double[p.npnts]; // time step t
+   double* rho_next = new double[p.npnts]; // time step t+1
+   double* x      = new double[p.npnts]; // x values
 
    // Initialize
-   for (int i = 0; i < npnts; i++) {
-      x[i] = p.x1 + ((i-1)*(p.x2-p.x1))/ngrid; 
+   for (int i = 0; i < p.npnts; i++) {
+      x[i] = p.x1 + ((i-1)*(p.x2-p.x1))/p.ngrid; 
       rho[i] = 0.0;
       rho_prev[i] = 0.0;
       rho_next[i] = 0.0;
    }
 
    // Excite
-   for (int i = npnts/4 + 1; i < 3*npnts/4; i++) {
-      rho[i] = 0.25 - fabs(double(i-npnts/2)/double(npnts));
+   for (int i = p.npnts/4 + 1; i < 3*p.npnts/4; i++) {
+      rho[i] = 0.25 - fabs(double(i-p.npnts/2)/double(p.npnts));
       rho_prev[i] = rho[i];
    }
 
    // Output initial signal
    std::cout << "\n#t = " << 0 << "\n";
-   for (int i = 1; i <= ngrid; i++) 
+   for (int i = 1; i <= p.ngrid; i++) 
       std::cout << x[i] << " " << rho[i] << "\n";
 
    // Take timesteps
-   for (int sc = 0; sc < nsteps; sc++) {
+   for (int sc = 0; sc < p.nsteps; sc++) {
 
       // Set zero dirichlet boundary conditions
       rho[0] = 0.0;
-      rho[ngrid+1] = 0.0;
+      rho[p.ngrid+1] = 0.0;
 
       // Evolve
-      for (int i = 1; i <= ngrid; i++) {
-         double laplacian = pow(p.c/s.dx,2)*(rho[i+1] + rho[i-1] - 2*rho[i]);
+      for (int i = 1; i <= p.ngrid; i++) {
+         double laplacian = pow(p.c/p.dx,2)*(rho[i+1] + rho[i-1] - 2*rho[i]);
          double friction = (rho[i] - rho_prev[i])/p.tau;
-         rho_next[i] = 2*rho[i] - rho_prev[i] + dt*(laplacian*dt-friction);
+         rho_next[i] = 2*rho[i] - rho_prev[i] + p.dt*(laplacian*p.dt-friction);
       }
 
       // Rotate array pointers so t+1 becomes the new t etc.
@@ -146,18 +147,18 @@ int main(int argc, char* argv[])
       rho_next = temp;  
 
       // Output density
-      if ((sc+1)%nper == 0) {
-         std::cout << "\n\n# t = " << sc*dt << "\n";
-         for (int i = 1; i <= ngrid; i++) 
+      if ((sc+1)%p.nper == 0) {
+         std::cout << "\n\n# t = " << sc*p.dt << "\n";
+         for (int i = 1; i <= p.ngrid; i++) 
             std::cout << x[i] << " " << rho[i] << "\n";
       }
    }
 
    // Deallocate memory
-//   delete[] rho_prev;
-//   delete[] rho;
-//   delete[] rho_next;
-//   delete[] x;
+   delete[] rho_prev;
+   delete[] rho;
+   delete[] rho_next;
+   delete[] x;
 
    return 0;
 }
