9d0fe2050ec39e6489a70b544876d17f433d3cb6
Peeter Joot <peeterjoot@protonmail.com>
2016-01-28 20:56:22 -0800

    modified:   run.sh

--- phy1610/ps3/run.sh
+++ phy1610/ps3/run.sh
@@ -4,3 +4,14 @@
 #make profile.orig.txt 
 (time ./origants) 2>&1 | tail -3 | tee time.orig.out
 (time ./ants) 2>&1 | tail -3 | tee time.mine.out
+
+
+# mine (mine is probably a bit faster since there's a double counting of totants in the original)
+real	0m0.716s
+user	0m0.696s
+sys	0m0.000s
+
+# orig
+real	0m0.726s
+user	0m0.692s
+sys	0m0.000s
