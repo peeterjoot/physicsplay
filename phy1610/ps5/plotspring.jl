7a9762c54d7ad7eaa9261ab29834ee961cfae087
Peeter Joot <peeterjoot@protonmail.com>
2016-02-22 07:11:20 -0800

    modified:   plotspring.jl modified:   plotspring2.jl modified:   springmodel.cc
       merge plotspring2.jl into plotspring.jl and add options to compare to the springmodel.cc gsl function

--- phy1610/ps5/plotspring.jl
+++ phy1610/ps5/plotspring.jl
@@ -7,9 +7,12 @@ c = 100 ; #N/m (spring constant)
 d = 0.5 ; #m(maximum spring extension)
 f = 2500 ; #(stiffness at maximum extension)
 g = 9.8 ; #m/s^2 (gravitational accelleration).
+c1 = a * b ;
+c2 = a * d^2/f ;
+c3 = -(c/2/a) ;
 
 function E_s(x)
-   (a * b) ./ x + (a * d^2/f) ./ ( (d - x).^2 ) - a * exp( -(c/2/a) * (x - b).^2 )  
+   c1 ./ x + c2 ./ ( (d - x).^2 ) - a * exp( c3 * (x - b).^2 )  
 end
 
 function E_w(m, x)
@@ -18,17 +21,25 @@ end
 
 m = 0.1 ;
 e = 0.01 ;
-xrange = collect( e:d/1000:d-e ) ;
-y = E_s(xrange) + E_w( m, xrange) ;
+n = 1000 ;
+xrange = collect( e:d/n:d-e ) ;
+y = E_s( xrange ) + E_w( m, xrange) ;
 
-xsz = length(xrange) ;
-xd = xrange[1:xsz-1]
-di = Array( Float64, xsz ) ;    
-# there's probably a function for adjacent differences, but can't look it 
-# up while offline:
-for i in 1:xsz-1
-   di[i] = y[i+1]-y[i]
+run( `./springmodel -m $m -f x.csv -n $n` ) ;
+csv = readdlm( "x.csv", ',' ) ;
+run( `rm -f x.csv` ) ;
+csvx = csv[ :, 1 ] ;
+csvy = csv[ :, 2 ] ;
+
+if ( true )
+   plot( xrange, y, color="red", linewidth=2.0 ) 
+   plot( csvx, csvy, color="blue", linewidth=2.0 ) 
+else
+   xlen = length( xrange ) ;
+   clen = length( csvx ) ;
+   xd = xrange[1:xlen-1] + d/n/2 ;
+   xc = csvx[1:clen-1] + d/n/2 ;
+   
+   plot( xc, diff( csvy ), color="red", linewidth=2.0 ) 
+   plot( xd, diff( y ), color="blue", linewidth=2.0 ) 
 end
-
-plot( xrange, y, color="red", linewidth=2.0 ) 
-#plot( xrange, di, color="blue", linewidth=2.0 ) 
--- phy1610/ps5/springmodel.cc
+++ phy1610/ps5/springmodel.cc
@@ -2,6 +2,8 @@
  \file springmodel.cc
  */
 #include "integers.h"
+#include <fstream>
+#include <string>
 #include <iostream>
 #include <cstdio>
 #include <getopt.h>
@@ -25,10 +27,14 @@ void showHelpAndExit()
 {
    static_assert( (int)RETURNCODES::LAST <= 256, "exit code doesn't fit in waitpid waitstatus byte." ) ;
 
-   std::cerr << "usage: springmodel\n" 
-//      "\t[--gridsize=N|-g N]\n"
-//      "\t[--timeintervals=N|-t N]\n"
-      "\t[--help]" << std::endl ;
+   std::cerr 
+      << "usage: springmodel\n" 
+         "\t[--mass=m|-m m]\n"
+         "\t[--file=p|-f p]\n"
+         "\tWhen file is specified evaluate the minimization function at a\n"
+         "\tset of points in the interval and output a csv format file with these points.\n"
+         "\t[--help]" 
+      << std::endl ;
 
    std::exit( (int)RETURNCODES::HELP ) ;
 }
@@ -41,15 +47,19 @@ int main( int argc, char ** argv )
    int c{0} ;
    int line{0} ;
    double mass{0.1} ;
+   unsigned long num{1000} ;
+   std::string filename{} ;
 
    constexpr struct option long_options[]{
      { "help",           0, NULL, 'h' },
      { "mass",           1, NULL, 'm' },
+     { "file",           1, NULL, 'f' },
+     { "num",            1, NULL, 'n' },
      { NULL,             0, NULL, 0   }
    } ;
 
    try {
-      while ( -1 != ( c = getopt_long( argc, argv, "hm:", long_options, NULL ) ) )
+      while ( -1 != ( c = getopt_long( argc, argv, "hm:f:n:", long_options, NULL ) ) )
       { 
          switch ( c )
          {
@@ -69,10 +79,24 @@ int main( int argc, char ** argv )
                break ;
             }
 #endif
+            case 'f' :
+            {
+               line = __LINE__ ;
+               filename = std::string( optarg ) ;
+
+               break ;
+            }
             case 'm' :
             {
                line = __LINE__ ;
                mass = std::stod( optarg ) ;
+
+               break ;
+            }
+            case 'n' :
+            {
+               line = __LINE__ ;
+               num = std::stoul( optarg ) ;
 
                break ;
             }
@@ -100,22 +124,30 @@ int main( int argc, char ** argv )
    minimizerParameters p( mass ) ;
    minimizerResults r ;
 
-   for ( double x = 0.01 ; x < 0.5 ; x += 0.01 )
+   if ( filename.length() )
    {
-      printf( "%f, %f\n", x, p.m_f( x ) ) ;
-   }
-#if 0
-   f_min_all( p, r ) ;
+      std::ofstream f( filename.c_str() ) ;
 
-   std::cout << "Using " << r.m_solvername << " on: [ " << p.m_a << ", " << p.m_b << " ]\n"
-             << "Iterations:\t" << r.m_iter << "\n"
-             << "Converged:\t" << r.m_converged << "\n"
-             << "Status:\t" << r.m_status << " (" << r.m_strerror << ")" << "\n"
-             << "Min:\t" << r.m_min << "\n"
-             << "F(Root):\t" << p.m_f(r.m_min) << "\n"
-             << "Abserr (bracket):\t" << r.m_b - r.m_a << "\n"
-             << std::endl ;
-#endif
+      double e = 0.01 ;
+
+      for ( double x = e ; x < p.m_f.d - e ; x += p.m_f.d/num )
+      {
+         f << x << ", " << p.m_f( x ) << std::endl ;
+      }
+   }
+   else
+   {
+      f_min_all( p, r ) ;
+
+      std::cout << "Using " << r.m_solvername << " on: [ " << p.m_a << ", " << p.m_b << " ]\n"
+                << "Iterations:\t" << r.m_iter << "\n"
+                << "Converged:\t" << r.m_converged << "\n"
+                << "Status:\t" << r.m_status << " (" << r.m_strerror << ")" << "\n"
+                << "Min:\t" << r.m_min << "\n"
+                << "F(Root):\t" << p.m_f(r.m_min) << "\n"
+                << "Abserr (bracket):\t" << r.m_b - r.m_a << "\n"
+                << std::endl ;
+   }
 
    return (int)RETURNCODES::SUCCESS ;
 }
