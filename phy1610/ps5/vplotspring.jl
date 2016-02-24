e305d615c7200daac0c93978e19e18769e33f5dd
Peeter Joot <peeterjoot@protonmail.com>
2016-02-23 20:39:26 -0800

    modified:   vplotspring.jl
       plot three plots, all in the x = [0,0.5] range.
    
       1) the energy function for some different mass values
       2) the adjacent differences of the energy function values from plot 1.
       3) the sign of these adjacent differences from plot 2.
    
       See from this (plot 3) that that there is a pair of local minimums even for m=0.001, which
       was hard to see from plot 1 or 2.  My partition of the intervals isn't finding these low mass
       local minimums in all cases (for small masses).

--- phy1610/ps5/vplotspring.jl
+++ phy1610/ps5/vplotspring.jl
@@ -7,25 +7,49 @@ e1 = 0.075 ;
 e2 = 0.02 ;
 n = 1000 ;
 xrange = collect( e1:d/n:d-e2 ) ;
-plotabs = true ;
-for i in 1:length(masses)
-   m = masses[i] ;
-   y = E_s( xrange ) + E_w( m, xrange) ;
 
-   if ( plotabs )
-      plot( xrange, y, label="m = $m" ) ;
-   else
-      xd = xrange[1:end-1] + d/n/2 ;
-      plot( xd, diff( y ), color=colors[i] ) ;
+for plotabs in 0:2
+   figure() ;
+
+   for i in 1:length(masses)
+      m = masses[i] ;
+      y = E_s( xrange ) + E_w( m, xrange) ;
+
+      if ( 0 == plotabs )
+         plot( xrange, y, label="m = $m" ) ;
+      else
+         xd = xrange[1:end-1] + d/n/2 ;
+         dy = diff( y ) ;
+         if ( plotabs == 1 )
+            plot( xd, dy, label="m = $m" ) ;
+         else
+            plot( xd, sign(dy) + i/10, label="m = $m" ) ;
+         end
+      end
    end
-end
 
-if ( plotabs )
    xlabel("x")
-   ylabel(L"$E_{tot}$")
-   title(L"Nonlinear spring, $E_{tot}$ vs position.")
-   legend( loc="lower left" ) ;
    grid("on")
 
-   savefig("energyVsPositionFig1.pdf")
+   if ( 0 == plotabs )
+      ylabel(L"$E_{tot}$")
+      title(L"Nonlinear spring, $E_{tot}$ vs position.")
+      legend( loc="lower left" ) ;
+
+      savefig("energyVsPositionFig1.pdf")
+   else
+      if ( 1 == plotabs )
+         ylabel(L"$d E_{tot}$")
+         title(L"Nonlinear spring, $d E_{tot}$ vs position.")
+         legend( loc="lower right" ) ;
+
+         savefig("DeltaEnergyVsPositionFig1.pdf")
+      else
+         ylabel(L"$sgn(d E_{tot})$")
+         title(L"Nonlinear spring, $sgn(d E_{tot})$ vs position.")
+         legend( loc="lower right" ) ;
+
+         savefig("SignDeltaEnergyVsPositionFig1.pdf")
+      end
+   end
 end
