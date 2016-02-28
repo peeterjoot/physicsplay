c6763399f75b8da93728f198db22a64ab5d5305e
Peeter Joot <peeterjoot@protonmail.com>
2016-02-28 10:24:27 -0800

    new file:   ../../bin/myindent new file:   install.txt new file:   writefile.cc

---
+++ phy1610/netcdf/writefile.cc
@@ -0,0 +1,37 @@
+// netCDF.writing.cpp
+#include <vector>
+#include <netcdf.h>
+
+using namespace netCDF ;
+
+int main()
+{
+   int nx = 6, ny = 12 ;
+   int dataOut[nx][ny] ;
+   for ( int i = 0 ; i < nx; i++ )
+   {
+      for ( int j = 0 ; j < ny; j++ )
+      {
+         dataOut[i][j] = i * ny + j ;
+      }
+   }
+
+   // Create the netCDF file.
+   NcFile dataFile( "first.netCDF.nc", NcFile::replace ) ;
+   // Create the two dimensions.
+   NcDim xDim = dataFile.addDim( "x", nx ) ;
+   NcDim yDim = dataFile.addDim( "y", ny ) ;
+   std::vector < NcDim > dims( 2 ) ;
+   dims[0] = xDim ;
+   dims[1] = yDim ;
+
+   // Create the data variable.
+   NcVar data = dataFile.addVar( "data", NcInt, dims ) ;
+   // Put the data in the file.
+   data.putVar( &dataOut ) ;
+
+   // Add an attribute.
+   dataFile.putAtt( "Creation date:", "Feb 28, 2016" ) ;
+
+   return 0 ;
+}
