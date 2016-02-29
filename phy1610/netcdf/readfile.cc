77faed75dfd90cc916e0646988bbc29355d33273
Peeter Joot <peeterjoot@protonmail.com>
2016-02-28 20:29:35 -0800

    modified:   GNUmakefile new file:   readfile.cc

---
+++ phy1610/netcdf/readfile.cc
@@ -0,0 +1,45 @@
+// netCDF.reading.cpp
+#include <iostream>
+#include <netcdf>
+
+using namespace netCDF ;
+
+int main()
+{
+   // Specify the netCDF file.
+   NcFile dataFile( "first.netCDF.nc", NcFile::read ) ;
+
+   // Read the two dimensions.
+   NcDim xDim = dataFile.getDim( "x" ) ;
+   NcDim yDim = dataFile.getDim( "y" ) ;
+
+   int nx = xDim.getSize() ;
+   int ny = yDim.getSize() ;
+
+   std::cout << "Our matrix is " << nx << " by " << ny << std::endl ;
+
+   int **p = new int *[nx] ;
+   p[0] = new int[nx * ny] ;
+
+   for ( int i = 0 ; i < nx; i++ )
+   {
+      p[i] = &p[0][i * ny] ;
+   }
+
+   // Create the data variable.
+   NcVar data = dataFile.getVar( "data" ) ;
+
+   // Put the data in a variable.
+   data.getVar( p[0] ) ;
+   for ( int i = 0 ; i < nx; i++ )
+   {
+      for ( int j = 0 ; j < ny; j++ )
+      {
+         std::cout << p[i][j] << " " ;
+      }
+
+      std::cout << std::endl ;
+   }
+
+   return 0 ;
+}
