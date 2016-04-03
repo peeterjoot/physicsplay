97d85720f4fc22d3c8ffd8a5a68ae62b6a1b5339
Peeter Joot <peeterjoot@protonmail.com>
2016-04-03 00:23:33 -0400

    modified:   GNUmakefile modified:   assignment.txt modified:   how new file:   leftright.cc new file:   nocycle.cc modified:   wave1d.cc
       construct two simple mpi samples, one that does cyclic comm and another that terminates
       at the boundaries.

---
+++ phy1610-scientific-computing/ps10/leftright.cc
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char* argv[])
+{
+   int rank ;
+   int size ;
+   int err ;
+   double msgsent, msgrcvd ;
+   MPI_Status rstatus ;
+   int tag = 1 ;
+
+   err = MPI_Init( &argc, &argv ) ;
+   if ( err )
+   {
+      printf( "init err: %d\n", err ) ;
+      abort() ;
+   }
+   err = MPI_Comm_size( MPI_COMM_WORLD, &size ) ;
+   if ( err )
+   {
+      printf( "size err: %d\n", err ) ;
+      abort() ;
+   }
+   err = MPI_Comm_rank( MPI_COMM_WORLD, &rank ) ;
+   if ( err )
+   {
+      printf( "rank err: %d\n", err ) ;
+      abort() ;
+   }
+
+   msgsent = rank * rank ;
+   msgrcvd = -999. ;
+
+   int left = (rank - 1 + size) % size ;
+   int right = (rank + 1) % size ;
+
+   err = MPI_Sendrecv( &msgsent, 1, MPI_DOUBLE, right, tag,
+                       &msgrcvd, 1, MPI_DOUBLE, left, tag,
+                       MPI_COMM_WORLD, &rstatus ) ;
+   if ( err )
+   {
+      printf( "sendrecv err = %d\n", err ) ;
+      abort() ;
+   }
+
+   std::cout << "Hello from task.  Rank = " << rank << ", size = " << size << " world.  msgsent = " << msgsent << ", msgrcvd = " << msgrcvd << "\n" ;
+ 
+   err = MPI_Finalize() ;
+   if ( err )
+   {
+      printf( "finalize err = %d\n", err ) ;
+      abort() ;
+   }
+
+   return 0 ;
+}
--- phy1610-scientific-computing/ps10/wave1d.cc
+++ phy1610-scientific-computing/ps10/wave1d.cc
@@ -19,9 +19,11 @@ int main(int argc, char* argv[])
    int rank ;
    int size ;
    int err ;
+#if 0
    double msgsent, msgrcvd ;
    MPI_Status rstatus ;
    int tag = 1 ;
+#endif
 
    err = MPI_Init( &argc, &argv ) ;
    if ( err )
@@ -41,26 +43,6 @@ int main(int argc, char* argv[])
       printf( "rank err: %d\n", err ) ;
       abort() ;
    }
-
-   msgsent = rank * rank ;
-   msgrcvd = -999. ;
-
-   int left = (rank - 1 + size) % size ;
-   int right = (rank + 1) % size ;
-
-   err = MPI_Sendrecv( &msgsent, 1, MPI_DOUBLE, right, tag,
-                  &msgrcvd, 1, MPI_DOUBLE, left, tag,
-                  MPI_COMM_WORLD, &rstatus ) ;
-   if ( err )
-   {
-      printf( "sendrecv err = %d\n", err ) ;
-      abort() ;
-   }
- 
-   std::cout << "Hello from task.  Rank = " << rank << ", size = " << size << " world.  msgsent = " << msgsent << ", msgrcvd = " << msgrcvd << "\n" ;
- 
-   err = MPI_Finalize() ;
-   return 0 ;
 
    // Open inifile and parse (using Inifile class from inifile.h)
    Inifile parameter(argc==1?"default.txt":argv[1]) ;
@@ -211,6 +193,13 @@ int main(int argc, char* argv[])
    {
       dataFile.close() ;
    }
-   
+  
+   err = MPI_Finalize() ;
+   if ( err )
+   {
+      printf( "rank err: %d\n", err ) ;
+      abort() ;
+   }
+
    return 0 ;
 }
