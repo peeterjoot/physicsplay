1874068e3e7af4d817fd5a2cf89af66716eadcf5
Peeter Joot <peeterjoot@protonmail.com>
2016-04-04 22:04:02 -0400

    modified:   wave1d.cc
    bail after first timestep.
    make graphics=0 the default.
    some formatting changes for consistency.

--- phy1610-scientific-computing/ps10/wave1d.cc
+++ phy1610-scientific-computing/ps10/wave1d.cc
@@ -1,7 +1,7 @@
 //
 // wave1d.cc - Simulates a one-dimensional damped wave equation modified
-// to use pgplot for runtime display of solution with 1 second between frames.  
-// 
+// to use pgplot for runtime display of solution with 1 second between frames.
+//
 // SciNet - March 2015
 
 #include <iostream>
@@ -14,7 +14,7 @@
 #include "inifile.h"
 #include <mpi.h>
 
-int main(int argc, char* argv[])
+int main( int argc, char* argv[] )
 {
    int rank ;
    int size ;
@@ -48,29 +48,29 @@ int main(int argc, char* argv[])
    Inifile parameter(argc==1?"default.txt":argv[1]) ;
 
    // Physical parameters
-   double  c      = parameter.get<double>("c", 1.0);    // wave speed
-   double  tau    = parameter.get<double>("tau", 20.0);  // damping time
-   double  x1     = parameter.get<double>("x1", -26.0);  // left most x value
-   double  x2     = parameter.get<double>("x2", +26.0);  // right most x value
+   double  c      = parameter.get<double>("c", 1.0) ;    // wave speed
+   double  tau    = parameter.get<double>("tau", 20.0) ;  // damping time
+   double  x1     = parameter.get<double>("x1", -26.0) ;  // left most x value
+   double  x2     = parameter.get<double>("x2", +26.0) ;  // right most x value
 
    // Simulation parameters
-   double  runtime = parameter.get<double>("runtime", 50.0);   // how long should the simulation try to compute?
-   double  dx     = parameter.get<double>("dx", 0.01);      // spatial grid size  //0.02
+   double  runtime = parameter.get<double>("runtime", 50.0) ;   // how long should the simulation try to compute?
+   double  dx     = parameter.get<double>("dx", 0.01) ;      // spatial grid size  //0.02
 
    // Output parameters
-   double  outtime =  parameter.get<double>("outtime", 1.0); // how often should a snapshot of the wave be written out? 
+   double  outtime =  parameter.get<double>("outtime", 1.0) ; // how often should a snapshot of the wave be written out?
 
-   bool   graphics = parameter.get<bool>("graphics", true);   // output to graphics (with 1 sec delay)  or to a file?
+   bool   graphics = parameter.get<bool>("graphics", false) ;   // output to graphics (with 1 sec delay)  or to a file?
 
    // Output file name
    const std::string dataFilename = "dataFilename.out" ;
 
    // Derived parameters
-   int    ngrid   = (x2-x1)/dx;  // number of x points
-   int    npnts   = ngrid + 2;   // number of x points including boundary points
-   double  dt     = 0.5*dx/c;   // time step size
-   int    nsteps  = runtime/dt;  // number of steps of that size to reach runtime
-   int    nper   = outtime/dt;  // how many step s between snapshots
+   int    ngrid   = (x2-x1)/dx ;  // number of x points
+   int    npnts   = ngrid + 2 ;   // number of x points including boundary points
+   double  dt     = 0.5*dx/c ;   // time step size
+   int    nsteps  = runtime/dt ;  // number of steps of that size to reach runtime
+   int    nper   = outtime/dt ;  // how many step s between snapshots
 
    // Report all the values.
    std::cout << "#c      " << c      << std::endl ;
@@ -87,14 +87,15 @@ int main(int argc, char* argv[])
    std::cout << "#graphics " << int(graphics) << std::endl ;
 
    // Define and allocate arrays.
-   rarray<float,1> rho_prev(npnts); // time step t-1
-   rarray<float,1> rho(npnts);     // time step t
-   rarray<float,1> rho_next(npnts); // time step t+1
-   rarray<float,1> rho_init(npnts); // initial values
-   rarray<float,1> x(npnts);      // x values
- 
+   rarray<float,1> rho_prev(npnts) ; // time step t-1
+   rarray<float,1> rho(npnts) ;     // time step t
+   rarray<float,1> rho_next(npnts) ; // time step t+1
+   rarray<float,1> rho_init(npnts) ; // initial values
+   rarray<float,1> x(npnts) ;      // x values
+
    // Initialize.
-   for (int i = 0; i < npnts; i++) {
+   for ( int i = 0 ; i < npnts ; i++ )
+   {
       x[i] = x1 + ((i-1)*(x2-x1))/ngrid ;
       rho[i] = 0.0 ;
       rho_prev[i] = 0.0 ;
@@ -102,53 +103,72 @@ int main(int argc, char* argv[])
    }
 
    // Excite.
-   for (int i = npnts/4 + 1; i < 3*npnts/4; i++) {
+   for ( int i = npnts/4 + 1 ; i < 3*npnts/4 ; i++ )
+   {
       rho[i] = 0.25 - fabs(float(i-npnts/2)/float(npnts)) ;
       rho_prev[i] = rho[i] ;
       rho_init[i] = rho[i] ;
    }
-      
+
    // Plot or Write out data.
    std::ofstream dataFile ;
    int red, grey, white ;
 
-   if (graphics) {
+   if ( graphics )
+   {
       cpgbeg(0, "/xwindow", 1, 1) ;
       cpgask(0) ;
-      red = 2; cpgscr(red,1.,0.,0.) ;
-      grey = 3; cpgscr(grey,.2,.2,.2) ;
-      white = 4; cpgscr(white,1.0,1.0,1.0) ;
-      cpgsls(1); cpgslw(6); cpgsci(white) ;
+      red = 2 ; cpgscr(red,1.,0.,0.) ;
+      grey = 3 ; cpgscr(grey,.2,.2,.2) ;
+      white = 4 ; cpgscr(white,1.0,1.0,1.0) ;
+      cpgsls(1) ; cpgslw(6) ; cpgsci(white) ;
       cpgslw(2) ;
       cpgenv(x1, x2, 0., 0.25, 0, 0) ;
       cpglab("x", "rho", "Wave Test") ;
-      cpgsls(1); cpgslw(6); cpgsci(white) ;
+      cpgsls(1) ; cpgslw(6) ; cpgsci(white) ;
       cpgline(npnts, x.data(), rho.data()) ;
-      cpgsls(2); cpgslw(12); cpgsci(red) ;
+      cpgsls(2) ; cpgslw(12) ; cpgsci(red) ;
       cpgline(npnts, x.data(), &rho_init[0]) ;
-   } else {    
-      dataFile.open(dataFilename.c_str()) ;
-      dataFile << nper << ","   
-            << npnts      << "\n" ;
-      dataFile << 0.0 << "\n" ;
-      for (int i = 0; i < npnts; i++ ) 
-        dataFile << x[i] << " " << rho[i] << " \n" ;
-      dataFile << "\n" ;
    }
-    
+   else
+   {
+      dataFile.open( dataFilename.c_str() ) ;
+
+      if ( rank == 0 )
+      {
+         dataFile << nper << ','
+                  << npnts      << '\n' ;
+         dataFile << 0.0 << '\n' ;
+
+         for ( int i = 0 ; i < npnts ; i++ )
+         {
+            dataFile << x[i] << '\n' ;
+         }
+
+         for ( int i = 0 ; i < npnts ; i++ )
+         {
+            dataFile << rho[i] << '\n' ;
+         }
+
+         dataFile << '\n' ;
+      }
+   }
+
    // measure time
    TickTock tt ;
    tt.tick() ;
-   
+
    // Take timesteps
-   for (int s = 0; s < nsteps; s++) {
+   for ( int s = 0 ; s < nsteps ; s++ )
+   {
 
       // Set zero dirichlet boundary conditions
       rho[0] = 0.0 ;
       rho[ngrid+1] = 0.0 ;
 
       // Evolve
-      for (int i = 1; i <= ngrid; i++) {
+      for ( int i = 1 ; i <= ngrid ; i++ )
+      {
          float laplacian = pow(c/dx,2)*(rho[i+1] + rho[i-1] - 2*rho[i]) ;
          float friction = (rho[i] - rho_prev[i])/tau ;
          rho_next[i] = 2*rho[i] - rho_prev[i] + dt*(laplacian*dt-friction) ;
@@ -162,38 +182,47 @@ int main(int argc, char* argv[])
       rho_next = temp ;
 	
       //Output every nper
-      if ((s+1)%nper == 0) {
-         if (graphics) {
-           cpgbbuf() ;
-           cpgeras() ;
-           cpgsls(1); cpgslw(6); cpgsci(white) ;
-           cpgslw(2) ;
-           cpgenv(x1, x2, 0., 0.25, 0, 0) ;
-           cpglab("x", "rho", "Wave test");  //t=s*dt
-           cpgsls(2); cpgslw(12); cpgsci(red) ;
-           cpgline(npnts, x.data(), rho.data()) ;
-           cpgsls(1); cpgslw(6); cpgsci(white) ;
-           cpgline(npnts, x.data(), rho_init.data()) ;
-           cpgebuf() ;
-           sleep(1); // artificial delay! 
-         } else {
-           dataFile << s * dt << "\n" ;
-           for (int i = 0; i < npnts; i++ ) 
-             dataFile<< x[i] << " " << rho[i] << "\n" ;
-           dataFile << "\n" ;
-         } 
+//      if ((s+1)%nper == 0)
+      {
+         if (graphics)
+         {
+            cpgbbuf() ;
+            cpgeras() ;
+            cpgsls(1) ; cpgslw(6) ; cpgsci(white) ;
+            cpgslw(2) ;
+            cpgenv(x1, x2, 0., 0.25, 0, 0) ;
+            cpglab("x", "rho", "Wave test") ;  //t=s*dt
+            cpgsls(2) ; cpgslw(12) ; cpgsci(red) ;
+            cpgline(npnts, x.data(), rho.data()) ;
+            cpgsls(1) ; cpgslw(6) ; cpgsci(white) ;
+            cpgline(npnts, x.data(), rho_init.data()) ;
+            cpgebuf() ;
+            sleep(1) ; // artificial delay!
+         }
+         else
+         {
+            dataFile << s * dt << '\n' ;
+
+            for ( int i = 0 ; i < npnts ; i++ )
+            {
+               dataFile << rho[i] << '\n' ;
+            }
+
+            dataFile << '\n' ;
+         }
       }
+break ;
    }
-   
+
    // Output measured runtime.
    std::cout << "Walltime = " << tt.silent_tock() << " sec."  << std::endl ;
 
    // Close file.
-   if (not graphics)
+   if ( not graphics )
    {
       dataFile.close() ;
    }
-  
+
    err = MPI_Finalize() ;
    if ( err )
    {
