8e68b7416aea1cfca5175610d6f1150d3d4ca9fe
Peeter Joot <peeterjoot@protonmail.com>
2016-04-07 13:55:13 -0400

    modified:   compareoutput.sh
    ./wave1d gets mpi errors when run directly.
    
    Run as: 'mpirun -n 1 wave1d' instead.  This wasn't required on my ubuntu system.

--- phy1610-scientific-computing/ps10/compareoutput.sh
+++ phy1610-scientific-computing/ps10/compareoutput.sh
@@ -1,12 +1,12 @@
 
 #pat="init:"
 pat="0:"
-for i in 2 5 10 ; do
-   rm -f dataFilename*out
-   ./wave1d waveparams.txt >/dev/null
-   grep $pat dataFilename0.out > x1
 
-   # with high enough contention (i=10) the output gets melded together.
+rm -f dataFilename*out
+mpirun -n 1 ./wave1d waveparams.txt >/dev/null
+grep $pat dataFilename0.out > x1
+
+for i in 2 5 10 ; do
    rm -f dataFilename*out
    mpirun -n $i ./wave1d  waveparams.txt >/dev/null
    cat dataFilename*out | grep $pat | sort -k2 -n -u > x2
