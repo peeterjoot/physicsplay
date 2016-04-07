86c615fcd782798d2ea6884e142550d006a0ccbb
Peeter Joot <peeterjoot@protonmail.com>
2016-04-06 18:02:10 -0700

    modified:   test.cc
       Add second partition test case to test more general input ranges.

--- phy1610-scientific-computing/ps10/test.cc
+++ phy1610-scientific-computing/ps10/test.cc
@@ -11,6 +11,9 @@
 #include "rangePartition.h"
 #include <vector>
 
+using s = rangePartition::subrange ;
+s empty{ 1, 0 } ;
+
 BOOST_AUTO_TEST_CASE( partitionTest1 )
 {
    constexpr auto size{4} ;
@@ -18,8 +21,6 @@ BOOST_AUTO_TEST_CASE( partitionTest1 )
    for ( auto extra{0} ; extra < 3 ; extra++ )
    {
       auto N{40 + extra} ;
-
-      using s = rangePartition::subrange ;
 
       s expected[] { {1, 10}, {11, 20}, {21, 30}, {31, N} } ;
 
@@ -41,6 +42,63 @@ BOOST_AUTO_TEST_CASE( partitionTest1 )
          {
             BOOST_REQUIRE( r.toLocalDomain( sub.second ) == 10 ) ;
          }
+      }
+   }
+}
+
+BOOST_AUTO_TEST_CASE( partitionTest2 )
+{
+   constexpr auto size{4} ;
+   constexpr auto N{41} ;
+
+   {
+      constexpr auto rank{0} ; // partition gets [1,10]
+
+      constexpr auto NTC{5} ;
+      s input[NTC]    { {1, 20}, {1, 10}, {5,10}, {5,15}, {20, N} } ;
+      s expected[NTC] { {1, 10}, {1, 10}, {5,10}, {5,10}, empty } ;
+
+      rangePartition r( N, size, rank ) ;
+
+      for ( auto i{0} ; i < NTC ; i++ )
+      {
+         s sub{ r.subsetOfGlobalRangeInThisPartition( input[i].first, input[i].second ) } ;
+
+         BOOST_REQUIRE( sub == expected[i] ) ;
+      }
+   }
+
+   {
+      constexpr auto rank{1} ; // partition gets [11,20]
+
+      constexpr auto NTC{6} ;
+      s input[NTC]    { {1, 10}, {5, 10}, {5, 15},  {10, 20}, {20, N}, {21, N} } ;
+      s expected[NTC] { empty,   empty,   {11, 15}, {11, 20}, {20,20}, empty } ;
+
+      rangePartition r( N, size, rank ) ;
+
+      for ( auto i{0} ; i < NTC ; i++ )
+      {
+         s sub{ r.subsetOfGlobalRangeInThisPartition( input[i].first, input[i].second ) } ;
+
+         BOOST_REQUIRE( sub == expected[i] ) ;
+      }
+   }
+
+   {
+      constexpr auto rank{3} ; // partition gets [30,41]
+
+      constexpr auto NTC{7} ;
+      s input[NTC]    { {1, 10}, {5, 10}, {5, 15},  {10, 20}, {20, N}, {31, N}, {29, 40} } ;
+      s expected[NTC] { empty,   empty,   empty,    empty,    {31, N}, {31, N}, {31, 40} } ;
+
+      rangePartition r( N, size, rank ) ;
+
+      for ( auto i{0} ; i < NTC ; i++ )
+      {
+         s sub{ r.subsetOfGlobalRangeInThisPartition( input[i].first, input[i].second ) } ;
+
+         BOOST_REQUIRE( sub == expected[i] ) ;
       }
    }
 }
