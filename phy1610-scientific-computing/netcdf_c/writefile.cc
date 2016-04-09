9719e401b897589bf3fb1202c146e39bef1f603f
Peeter Joot <peeterjoot@protonmail.com>
2016-04-08 20:48:38 -0700

    modified:   writefile.cc

--- phy1610-scientific-computing/netcdf_c/writefile.cc
+++ phy1610-scientific-computing/netcdf_c/writefile.cc
@@ -1,48 +1,77 @@
 #include <netcdf.h>
 #include <stdlib.h>
 #include <cstring>
+#include <vector>
+#include <iostream>
 #include "physicsplay_build_version.h"
 
-void handle_error( const int status )
-{
-   abort() ;
-}
+#define handle_error( status, what, line ) \
+do { \
+   if ( status ) \
+   { \
+      std::cout << __FILE__ << ':' << line << ':' << what << " failed with rc = " << status << ':' << nc_strerror( status ) << '\n' ; \
+      abort() ; \
+   } \
+} while ( 0 ) 
 
 int main()
 {
-   int status, ncid, idX, idTime ;
+   int status, ncid, idX, idTime, varid ;
+   constexpr auto N{40} ;
 
+   // also: nc_open for already existing only
    status = nc_create( "foo.nc",
                        NC_CLOBBER,
                        &ncid ) ;
-   if (status != NC_NOERR)
-      handle_error(status) ;
+   handle_error( status, "nc_create", __LINE__ ) ;
 
    status = nc_def_dim( ncid,
                         "X",
-                        40,
+                        N,
                         &idX ) ;
-   if (status != NC_NOERR)
-      handle_error(status) ;
+   handle_error( status, "nc_def_dim", __LINE__ ) ;
 
    status = nc_def_dim( ncid,
                         "T",
                         NC_UNLIMITED,
                         &idTime ) ;
-   if (status != NC_NOERR)
-      handle_error(status) ;
+   handle_error( status, "nc_def_dim", __LINE__ ) ;
+
+   int dims[2]{idTime, idX} ;
+
+   status = nc_def_var( ncid, "R", NC_INT, 2, dims, &varid);
+   handle_error( status, "nc_def_var", __LINE__ ) ;
+
+   std::vector<int> R ;
+   int i{0} ;
+   for( int &v : R )
+   {
+      v += 2 * i ;
+
+      i++ ;
+   }
 
    status = nc_put_att_text( ncid,
                              NC_GLOBAL,
                              "commit",
                              strlen(PHYSICSPLAY_COMMIT_INFO),
                              PHYSICSPLAY_COMMIT_INFO ) ;
-   if (status != NC_NOERR)
-      handle_error(status) ;
+   handle_error( status, "nc_put_att_text", __LINE__ ) ;
+
+   status = nc_enddef( ncid ) ;
+   handle_error( status, "nc_enddef", __LINE__ ) ;
+
+   size_t start{0} ;
+   size_t count{1} ;
+   status = nc_put_vara_int( ncid,
+                             varid,
+                             &start,
+                             &count,
+                             &R[0] );
+   handle_error( status, "nc_put_var_int", __LINE__ ) ;
 
    status = nc_close( ncid ) ;
-   if (status != NC_NOERR)
-      handle_error(status) ;
+   handle_error( status, "nc_close", __LINE__ ) ;
 
    return 0 ;
 }
