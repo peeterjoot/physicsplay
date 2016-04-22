42b4c0fae83708c35e9509bf83ef70d33decc17b
Peeter Joot <peeterjoot@protonmail.com>
2016-04-21 20:04:51 -0700

    modified:   bassert.cc

--- phy1610-scientific-computing/bassert/bassert.cc
+++ phy1610-scientific-computing/bassert/bassert.cc
@@ -1,11 +1,19 @@
 /** \file bassert.cc */
 #include <boost/exception/diagnostic_information.hpp>
 #include "myexceptions.h"
+#include <iostream>
 
 int main( int argc, char ** argv )
 {
    try {
-      ASSERT_DATA_INT( argc == 0, argc ) ;
+      auto expected{7} ;
+
+//      ASSERT_DATA_INT_INT( argc == expected, argc, expected ) ;
+::boost::exception_detail::throw_exception_(
+     assert_error() 
+         << intdata_info( argc ) 
+         << intdata2_info( expected )
+     ,__PRETTY_FUNCTION__,"bassert.cc",11) ;
    }
    catch ( boost::exception & e )
    {
