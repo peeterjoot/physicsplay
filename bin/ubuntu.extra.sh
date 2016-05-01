0ab1ed1da4413478f092c7011240462bd329af47
Peeter Joot <peeter.joot@lzlabs.com>
2016-05-01 17:12:23 -0400

    modified:   GNUmakefile modified:   bassert.cc

---
+++ bin/ubuntu.extra.sh
@@ -0,0 +1,31 @@
+apt install gparted
+apt install vim
+apt install openssh-server
+apt install screen
+apt install libsilo
+apt install netcdf-bin
+apt install openmpi-bin
+apt install openmpi-common
+apt install perl-doc perl-modules
+apt install rcs python python-3
+apt install rcs python 
+apt install xpdf
+apt install doxygen gawk gfortran graphviz indent julia
+apt install gsl
+apt install libblas-dev checkinstall
+apt install libblas-doc checkinstall
+apt install libfftw3-bin-dbgsym
+apt install libfftw3-dbg
+apt install libfftw3-dev
+apt install libfftw3-doc
+apt install libfftw3-double3-dbgsym
+apt install libfftw3-long3-dbgsym
+apt install libfftw3-single3-dbgsym
+apt install liblapack-doc checkinstall
+apt install liblapacke-dev checkinstall
+apt install libnetcdf-c++
+apt install libnetcdf-dev 
+apt install libsilo-bin
+apt install libsilo-dev
+apt install libstatistics-basic-perl
+apt install netcdf-bin
