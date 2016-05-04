7ad078395a39fb8b44b295e4d91e4a26fcad496e
Peeter Joot <peeter.joot@lzlabs.com>
2016-05-04 18:16:34 -0400

    modified:   GNUmakefile modified:   ../../mac.install.sh modified:   ../../make.vars
    mac porting.

--- phy1610-scientific-computing/mac.install.sh
+++ phy1610-scientific-computing/mac.install.sh
@@ -1,2 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install gsl
+brew install fftw
+brew install homebrew/science/openblas
+brew install homebrew/science/scalapack
