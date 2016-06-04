0ad9ae61b75aa904d4c65849f6e96162d7a34c35
Peeter Joot <peeter.joot@lzlabs.com>
2016-06-04 12:49:22 -0400

    modified:   is.cc

--- programming/isqrt/is.cc
+++ programming/isqrt/is.cc
@@ -13,6 +13,9 @@ constexpr int isqrt( int x )
 
 int main()
 {
+//   int k = 3/2 ;
+//   std::cout << k << '\n' ;
+
    for ( auto v : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 16, 25 } )
    {
 	   std::cout << "\\sqrt{" << v << "} = " << isqrt( v ) << '\n' ;
