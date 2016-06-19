258c16a97174ad5dae88ace1866e8233754eaad5
Peeter Joot <peeter.joot@lzlabs.com>
2016-06-19 09:16:48 -0400

    modified:   mac.install.sh modified:   ps9/GNUmakefile

--- phy1610-scientific-computing/mac.install.sh
+++ phy1610-scientific-computing/mac.install.sh
@@ -1,6 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install gsl
-brew install fftw
-brew install homebrew/science/openblas
-brew install homebrew/science/scalapack
-brew install netcdf
+
+for i in binutils boost fftw gcc gdb gmp gsl hdf5 homebrew/science/openblas homebrew/science/scalapack netcdf open-mpi openssl pgplot ; do
+   brew install $i
+done
