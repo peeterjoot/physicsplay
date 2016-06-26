edaf353988bd89e2a779e737b7a08160955cf437
Peeter Joot <peeter.joot@lzlabs.com>
2016-06-26 00:25:32 -0400

    modified:   ../../../notes/blogit/cpp11.4.txt new file:   d.cc new file:   makefile

---
+++ programming/stroustroupbook/commaop/d.cc
@@ -0,0 +1,24 @@
+struct foo
+{
+   int m ;
+
+   foo( int v = {} ) : m{v} {}
+
+   int blah( ) const
+   {
+      return m + 3 ;
+   }
+
+   int operator,(const foo & f) 
+   {
+      return blah() + f.blah() ;
+   }
+} ;
+
+int main()
+{
+   foo f ;
+   foo g{ 7 } ;
+
+   return f, g ;
+}
