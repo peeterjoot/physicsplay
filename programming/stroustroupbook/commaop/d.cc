5a0ba1f9d4c433c68ccf171d85b32f383f6cda48
Peeter Joot <peeter.joot@lzlabs.com>
2016-06-26 07:56:53 -0400

    modified:   d.cc

--- programming/stroustroupbook/commaop/d.cc
+++ programming/stroustroupbook/commaop/d.cc
@@ -14,11 +14,13 @@ struct foo
       return blah() + f.blah() ;
    }
 
+   // apparently this is also a valid operator.  Had no idea what it was, but stackoverflow says "pointer to member" dereference:
+   //
+   // http://stackoverflow.com/questions/38035231/what-is-the-c-operator
    int operator ->*(const foo & f) 
    {
       return blah() + f.blah() ;
    }
-} ;
 } ;
 
 int main()
@@ -26,6 +28,5 @@ int main()
    foo f ;
    foo g{ 7 } ;
 
-   //return f, g ;
-   return f ->* g ;
+   return (f, g) + (f ->* g) ;
 }
