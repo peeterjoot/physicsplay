d11964076f04d658369386afaf4ffe857711a8a4
Peeter Joot <peeter.joot@lzlabs.com>
2016-06-26 20:48:45 -0400

    modified:   ../../../notes/blogit/cpp11.4.txt modified:   d.cc

--- programming/stroustroupbook/literals/d.cc
+++ programming/stroustroupbook/literals/d.cc
@@ -1,12 +1,20 @@
 #include <string>
 #include <iostream>
 
+static_assert( __cplusplus >= 201402L, "require c++14 for string literal suffix" ) ;
+
 using namespace std::literals ;
 
 int main()
 {
    std::string hi { "hi\n" } ;
+#if 1
    hi += "there"s + "\n" ;
+#elif 0
+   hi += "there" + "\n" ;
+#else
+   hi += std::string{"there"} + "\n" ;
+#endif
 
    std::cout << hi ;
 
