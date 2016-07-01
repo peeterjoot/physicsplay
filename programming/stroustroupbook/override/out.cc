3f34babb8738f1046b193a6be9a4d9136f289640
Peeter Joot <peeter.joot@lzlabs.com>
2016-07-01 13:43:08 -0400

    modified:   out.cc

--- programming/stroustroupbook/override/out.cc
+++ programming/stroustroupbook/override/out.cc
@@ -19,3 +19,14 @@ void outofbounds( const uncheckedMatrix2 & m, const char * s ) noexcept
 {
    printf( "%s: %g\n", s, m(4,2) ) ;
 }
+
+// baseline:
+void outofbounds( const std::vector<double> m, const char * s ) noexcept
+{
+   printf( "%s: %g\n", s, m[ 4*3+2-1 ] ) ;
+}
+
+void outofbounds( const double * m, const char * s ) noexcept
+{
+   printf( "%s: %g\n", s, m[ 4*3+2-1 ] ) ;
+}
