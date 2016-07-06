60535a8b411c5ad56a87eba94570be073404df84
Peeter Joot <peeter.joot@lzlabs.com>
2016-07-06 09:55:47 -0400

    modified:   d.cc

--- programming/stroustroupbook/iter/d.cc
+++ programming/stroustroupbook/iter/d.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-// comment in the book that seems to imply <iterator> is required for begin() end() with builtin types?
+// comment in the book that seems to imply <iterator> is required for begin() end() with builtin types, but
+// I don't see that here.  What was that referring to?
 int main()
 {
    int x[]{ 1, 2,3,4} ;
