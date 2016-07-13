6a19cc62095e0cf6692dc13aa7175df5a5437030
Peeter Joot <peeter.joot@lzlabs.com>
2016-07-13 18:36:09 -0400

    modified:   ../../../notes/blogit/cpp11.7.txt modified:   d.cc modified:   makefile

--- programming/stroustroupbook/bitset/d.cc
+++ programming/stroustroupbook/bitset/d.cc
@@ -1,7 +1,66 @@
 #include <bitset>
 #include <iostream>
 
+#define WITHITER
+#if defined WITHITER
+template <int N>
+struct iter ;
+
+template <int N>
+struct mybits : public std::bitset<N>
+{
+   using T = std::bitset<N> ;
+
+   using T::T ;
+   using T::size ;
+
+   inline iter<N> begin( ) ;
+
+   inline iter<N> end( ) ;
+} ;
+
+template <int N>
+struct iter
+{
+   unsigned pos{} ;
+   const mybits<N> & b ;
+
+   iter( const mybits<N> & bits, unsigned p = {} ) : pos{p}, b{bits} {}
+
+   const iter & operator++()
+   {
+      pos++ ;
+
+      return *this ;
+   }
+
+   bool operator != ( const iter & i ) const
+   {
+      return pos != i.pos ;
+   }
+
+   int operator*() const
+   {
+      return b[ pos ] ;
+   }
+} ;
+
+template <int N>
+inline iter<N> mybits<N>::begin( )
+{
+   return iter<N>( *this ) ;
+}
+
+template <int N>
+inline iter<N> mybits<N>::end( )
+{
+   return iter<N>( *this, size() ) ;
+}
+
+using b70 = mybits<70> ;
+#else
 using b70 = std::bitset<70> ;
+#endif
 
 void foo( b70 & v )
 {
@@ -32,6 +91,15 @@ int main()
    // also have a built in output method, and various conversion, utility and test functions (count, any, all, none, size, ...)
    std::cout << v << '\n' ;
    std::cout << v.to_string() << '\n' ;
+   std::cout << "0x" << std::hex << v.to_ulong() << '\n' ;
+   //std::cout << (~v).to_ulong() << '\n' ; // throws overflow_error
+
+#if defined WITHITER
+   for ( auto b : v )
+   {
+      std::cout << b << '\n' ;
+   }
+#endif
 
    return 0 ;
 }
