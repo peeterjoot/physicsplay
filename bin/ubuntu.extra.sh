be5962d7adb886d6953860ffae391cefd99cbb1c
Peeter Joot <peeterjoot@protonmail.com>
2016-08-03 14:18:12 -0400

    modified:   ../bin/ubuntu.extra.sh modified:   make.vars
       windows 10 ubuntu subsystem updates.

--- bin/ubuntu.extra.sh
+++ bin/ubuntu.extra.sh
@@ -1,40 +1,40 @@
-apt install gparted
-apt install vim
-apt install openssh-server
-apt install screen
-apt install libsilo
-apt install netcdf-bin
-apt install openmpi-bin
-apt install openmpi-common
-apt install perl-doc perl-modules
-apt install rcs python python-3
-apt install rcs python 
-apt install xpdf
-apt install doxygen gawk gfortran graphviz indent julia
-apt install gsl
-apt install libblas-dev checkinstall
-apt install libblas-doc checkinstall
-apt install libfftw3-bin-dbgsym
-apt install libfftw3-dbg
-apt install libfftw3-dev
-apt install libfftw3-doc
-apt install libfftw3-double3-dbgsym
-apt install libfftw3-long3-dbgsym
-apt install libfftw3-single3-dbgsym
-apt install liblapack-doc checkinstall
-apt install liblapacke-dev checkinstall
-apt install libnetcdf-c++
-apt install libnetcdf-dev 
-apt install libsilo-bin
-apt install libsilo-dev
-apt install libstatistics-basic-perl
-apt install netcdf-bin
-apt install libboost-all-dev
-apt install libgsl0-dev
-apt install pgplot5
-apt install libnetcdf-dev libnetcdf11 netcdf-bin netcdf-dbg netcdf-doc
+apt install -y gparted
+apt install -y vim
+apt install -y openssh-server
+apt install -y screen
+apt install -y libsilo
+apt install -y netcdf-bin
+apt install -y openmpi-bin
+apt install -y openmpi-common
+apt install -y perl-doc perl-modules
+apt install -y rcs python python-3
+apt install -y rcs python 
+apt install -y xpdf
+apt install -y doxygen gawk gfortran graphviz indent julia
+apt install -y gsl
+apt install -y libblas-dev checkinstall
+apt install -y libblas-doc checkinstall
+apt install -y libfftw3-bin-dbgsym
+apt install -y libfftw3-dbg
+apt install -y libfftw3-dev
+apt install -y libfftw3-doc
+apt install -y libfftw3-double3-dbgsym
+apt install -y libfftw3-long3-dbgsym
+apt install -y libfftw3-single3-dbgsym
+apt install -y liblapack-doc checkinstall
+apt install -y liblapacke-dev checkinstall
+apt install -y libnetcdf-c++
+apt install -y libnetcdf-dev 
+apt install -y libsilo-bin
+apt install -y libsilo-dev
+apt install -y libstatistics-basic-perl
+apt install -y netcdf-bin
+apt install -y libboost-all-dev
+apt install -y libgsl0-dev
+apt install -y pgplot5
+apt install -y libnetcdf-dev libnetcdf11 netcdf-bin netcdf-dbg netcdf-doc
 
-apt install texlive-latex-base
-apt install texlive-latex-extra
-apt install texlive-full
+apt install -y texlive-latex-base
+apt install -y texlive-latex-extra
+apt install -y texlive-full
 
