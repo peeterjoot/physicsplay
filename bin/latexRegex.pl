9c96633c8232d33507f3374e6f47e886ac1000c9
Peeter Joot <peeter.joot@lzlabs.com>
2016-11-17 15:13:49 -0500

    modified:   ../../bin/latexRegex.pl modified:   GNUmakefile new file:   emtLecture10.tex modified:   renumber

--- bin/latexRegex.pl
+++ bin/latexRegex.pl
@@ -196,6 +196,7 @@ s/[Ss]chrodinger/Schr\\"{o}dinger/g; # Schrodinger
 #s/\\TB/\\tilde{\\BB}/g;
 #s/\\Te/\\hat{T}_{\\epsilon}/g ;
 #s/\\TE/\\tilde{\\BE}/g;
+s/\\TE/{\\textrm{TE}}/g;
 #s/_\\T/_{\\mathrm{T}}/g;
 #s/_T/_{\\mathrm{T}}/g ;
 #s/\\tmax\b/{\\textrm{max}}/g ;
