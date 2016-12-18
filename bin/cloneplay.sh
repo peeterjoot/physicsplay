335d997656f009f6b69401b0e93dbd7eda897723
Peeter Joot <peeter.joot@lzlabs.com>
2016-12-17 23:30:11 -0500

    Add notes/latex submodule

--- bin/cloneplay.sh
+++ bin/cloneplay.sh
@@ -1,5 +1,5 @@
 
-for i in getopt mathematica figures matlab clang julia ; do
+for i in getopt mathematica figures matlab clang julia notes/latex ; do
    git submodule update --init $i
    (cd $i && git checkout master)
 done
