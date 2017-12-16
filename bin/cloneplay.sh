24d813b031dbb162dc0d2f65475e3c02d21e4078
Peeter Joot <peeter.joot@lzlabs.com>
2017-12-16 13:33:06 -0500

    modified:   bin/cloneplay.sh

--- bin/cloneplay.sh
+++ bin/cloneplay.sh
@@ -1,5 +1,6 @@
 
-for i in getopt mathematica figures matlab clang julia notes/latex ; do
+#	mathematica 
+for i in getopt figures matlab clang julia notes/latex ; do
    git submodule update --init $i
    (cd $i && git checkout master)
 done
