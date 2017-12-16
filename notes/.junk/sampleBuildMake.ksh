2f91aab6030b553802420307ea296c601f7291a8
Peeter Joot <peeter.joot@lzlabs.com>
2017-12-16 13:36:52 -0500

    delete a bunch of stuff now migrated to separate repos.

--- notes/.junk/sampleBuildMake.ksh
+++
@@ -1,2 +0,0 @@
-cd ~/phyLatex/notes/phy454/
-make
--- notes/.phy487-qmsolids/pdfsz/u.bat
+++
@@ -1,5 +0,0 @@
-@rem export PATH=$PATH:/cygdrive/c/Users/Peeter/Downloads/sam2p-0.49-win32
-
-set PATH=C:\Users\Peeter\Downloads\sam2p-0.49-win32;C:\Users\Peeter\cygwin_home\pdfsizeopt
-
-C:\cygwin\bin\python2.6.exe C:\Users\Peeter\cygwin_home\pdfsizeopt --use-pngout=true --use-jbig2=true --use-multivalent=true --do-unify-fonts=false phy487.pdf
